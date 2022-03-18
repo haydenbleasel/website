1b8eeb4bde6c4f2b4bb572b6b09009b962c5ac8c
Hayden Bleasel <hello@haydenbleasel.com>
2022-03-18 20:06:31 +1100

    Add standard Next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,37 @@
+const { createSecureHeaders } = require("next-secure-headers");
+const withPlugins = require("next-compose-plugins");
+const withPWA = require("next-pwa");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: createSecureHeaders(),
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+const pwaConfig = {
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    dynamicStartUrl: false,
+    mode: process.env.NODE_ENV,
+  },
+};
+
+module.exports = withPlugins(
+  [[withPWA, pwaConfig], withBundleAnalyzer],
+  nextConfig
+);

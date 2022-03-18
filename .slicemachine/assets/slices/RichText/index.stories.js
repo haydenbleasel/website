57c39fd0498af05acf063fef107f6676e8605305
Hayden Bleasel <hello@haydenbleasel.com>
2022-03-18 20:48:30 +1100

    Build Case Study slice

---
+++ .slicemachine/assets/slices/RichText/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/RichText';
+
+export default {
+  title: 'slices/RichText'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"rich_text","items":[],"primary":{"content":[{"type":"paragraph","text":"Occaecat id adipisicing ad aute nostrud occaecat laboris.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'

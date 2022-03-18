2de8ce8f9fea0dfe4876596b041c06f53715c1f2
Hayden Bleasel <hello@haydenbleasel.com>
2022-03-18 20:23:13 +1100

    Scaffold Prismic utils, add Tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/typography"),
+  ],
+};
---
+++ utils/prismic.ts
@@ -0,0 +1,52 @@
+import * as prismic from "@prismicio/client";
+import type { LinkResolverFunction } from "@prismicio/helpers";
+import type {
+  FilledLinkToMediaField,
+  FilledLinkToWebField,
+  FilledLinkToDocumentField,
+} from "@prismicio/types";
+
+export const linkResolver: LinkResolverFunction = (document) => {
+  if (!document.uid) {
+    return "/";
+  }
+
+  const routes: Record<string, string> = {
+    home: "/",
+  };
+
+  return routes[document.type] || `/${document.uid}`;
+};
+
+export const docResolver = (
+  link:
+    | FilledLinkToDocumentField
+    | FilledLinkToWebField
+    | FilledLinkToMediaField
+): string => {
+  if (link.link_type === "Document") {
+    return linkResolver(link);
+  }
+
+  return link.url;
+};
+
+export const client = prismic.createClient(
+  process.env.PRISMIC_ENDPOINT ?? "loading",
+  {
+    fetch,
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN ?? "",
+  }
+);
+
+export const getPage = async (uid: string, type?: string): Promise<unknown> => {
+  const { data } = await client.getByUID(type ?? uid, uid);
+
+  return data;
+};
+
+export const getPages = async (type: string): Promise<unknown> => {
+  const pages = await client.getAllByType(type);
+
+  return pages;
+};

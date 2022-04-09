8aab52fec63ddace4331fdff6e686a4704ae5b9a
Hayden Bleasel <hello@haydenbleasel.com>
2022-04-09 19:11:29 +1000

    Fix typo

--- slices/DribbbleSlider/index.tsx
+++ slices/DribbbleSlider/index.tsx
@@ -85,7 +85,7 @@ const Shot: FC<{ shot: number }> = ({ shot }) => {
                   cy="12"
                   r="10"
                   stroke="currentColor"
-                  stroke-width="4"
+                  strokeWidth="4"
                 />
                 <path
                   className="opacity-75"

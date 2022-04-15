2b35fc7cab483a2f8ced08a25b5d0f494c48e41a
Hayden Bleasel <hello@haydenbleasel.com>
2022-04-15 22:20:48 +1000

    Add Games list

---
+++ pages/games.tsx
@@ -0,0 +1,97 @@
+import type { GetStaticProps } from 'next';
+import type { FC } from 'react';
+import { Fragment } from 'react';
+import type { KeyTextField, PrismicDocumentWithUID } from '@prismicio/types';
+import { Star } from 'react-feather';
+import { getPage } from '../utils/prismic';
+import Divider from '../components/divider';
+import Layout from '../components/layout';
+import type { AchievementProps, SteamGame } from '../utils/steam';
+import { getGames } from '../utils/steam';
+
+type ProjectsProps = {
+  data: {
+    title: KeyTextField;
+    description: KeyTextField;
+  };
+  games: {
+    game: SteamGame;
+    achievements: AchievementProps[];
+  }[];
+};
+
+const Achievements: FC<{ data: AchievementProps[] }> = ({ data }) => {
+  const completedAchievements = data.filter(({ achieved }) =>
+    Boolean(achieved)
+  );
+  const isPerfect = completedAchievements.length === data.length;
+
+  return (
+    <span
+      className={`ml-2 flex items-center gap-1 text-xs ${
+        isPerfect ? 'text-[#DC6803]' : 'text-gray-500 dark:text-gray-400'
+      }`}
+    >
+      <Star size={12} />
+      {completedAchievements.length} / {data.length}
+    </span>
+  );
+};
+
+const Game = ({ game, achievements }: ProjectsProps['games'][number]) => {
+  const { name, playtime_forever } = game;
+  const hours = Math.floor(playtime_forever / 60);
+
+  return (
+    <div className="flex flex-col gap-2 py-2 sm:flex-row sm:gap-8">
+      <p className="flex flex-1 items-center gap-2 text-md text-gray-900 dark:text-white">
+        {name}
+        {Boolean(achievements.length) && <Achievements data={achievements} />}
+      </p>
+      <p className="flex-0 flex w-24 text-sm text-gray-500 dark:text-gray-400 sm:justify-end">
+        {playtime_forever > 60
+          ? `${hours} ${hours === 1 ? 'hour' : 'hours'}`
+          : `${playtime_forever} ${
+              playtime_forever === 1 ? 'minute' : 'minutes'
+            }`}
+      </p>
+    </div>
+  );
+};
+
+const sortByPlaytime = (
+  gameA: ProjectsProps['games'][number],
+  gameB: ProjectsProps['games'][number]
+) => (gameB.game.playtime_forever > gameA.game.playtime_forever ? 1 : -1);
+
+const Games: FC<ProjectsProps> = ({ data, games }) => (
+  <Layout title={data.title} description={data.description}>
+    <div className="flex flex-col gap-4">
+      <p className="text-sm text-gray-500 dark:text-gray-400">
+        {data.description}
+      </p>
+      <div className="mt-4">
+        {games.sort(sortByPlaytime).map((game, index) => (
+          <Fragment key={index}>
+            {Boolean(index) && <Divider />}
+            <Game {...game} />
+          </Fragment>
+        ))}
+      </div>
+    </div>
+  </Layout>
+);
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = (await getPage('games')) as PrismicDocumentWithUID;
+  const games = await getGames();
+
+  return {
+    props: {
+      data,
+      games,
+    },
+  };
+};
+
+export default Games;
---
+++ utils/steam.ts
@@ -0,0 +1,73 @@
+export type SteamGame = {
+  appid: number;
+  name: string;
+  playtime_forever: number;
+  img_icon_url: string;
+  playtime_windows_forever: number;
+  playtime_mac_forever: number;
+  playtime_linux_forever: number;
+};
+
+export type PlayerStatsProps = {
+  playerstats:
+    | {
+        error: string;
+        success: false;
+      }
+    | {
+        steamID: string;
+        gameName: string;
+        achievements: AchievementProps[] | undefined;
+        success: true;
+      };
+};
+
+export type AchievementProps = {
+  apiname: string;
+  achieved: number;
+  unlocktime: number;
+};
+
+const getAchievements = async (appid: number): Promise<AchievementProps[]> => {
+  const response = await fetch(
+    `http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?key=${
+      process.env.STEAM_API_KEY ?? ''
+    }&steamid=${process.env.STEAM_ID ?? ''}&appid=${appid}`
+  );
+
+  const data = (await response.json()) as PlayerStatsProps;
+
+  if (!data.playerstats.success) {
+    return [];
+  }
+
+  return data.playerstats.achievements ?? [];
+};
+
+export const getGames = async (): Promise<
+  {
+    game: SteamGame;
+    achievements: AchievementProps[];
+  }[]
+> => {
+  const response = await fetch(
+    `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${
+      process.env.STEAM_API_KEY ?? ''
+    }&steamid=${process.env.STEAM_ID ?? ''}&include_appinfo=1`
+  );
+
+  const data = (await response.json()) as {
+    response: {
+      games: SteamGame[];
+    };
+  };
+
+  const games = data.response.games
+    .filter(({ playtime_forever }) => Boolean(playtime_forever))
+    .map(async (game) => ({
+      game,
+      achievements: await getAchievements(game.appid),
+    }));
+
+  return Promise.all(games);
+};

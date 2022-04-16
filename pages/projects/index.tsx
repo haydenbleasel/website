0e8077501802b2abe06348707a57115e33facd83
Hayden Bleasel <hello@haydenbleasel.com>
2022-04-16 18:30:36 +1000

    Add WIP projects

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -3,13 +3,16 @@ import type { FC } from 'react';
 import { Fragment } from 'react';
 import type {
   EmbedField,
+  GroupField,
   ImageFieldImage,
   KeyTextField,
+  LinkField,
   PrismicDocumentWithUID,
   SliceZone,
 } from '@prismicio/types';
 import { PrismicLink } from '@prismicio/react';
-import { getPage, getPages } from '../../utils/prismic';
+import Link from 'next/link';
+import { docResolver, getPage, getPages } from '../../utils/prismic';
 import Divider from '../../components/divider';
 import Layout from '../../components/layout';
 
@@ -17,6 +20,11 @@ type ProjectsProps = {
   data: {
     title: KeyTextField;
     description: KeyTextField;
+    wip: GroupField<{
+      name: KeyTextField;
+      description: KeyTextField;
+      link: LinkField;
+    }>;
   };
   projects: ProjectProps[];
 };
@@ -29,34 +37,61 @@ type ProjectProps = PrismicDocumentWithUID<{
   slices1: SliceZone;
 }>;
 
-const Project = (project: ProjectProps) => {
-  const { id, data } = project;
-
-  return (
-    <PrismicLink document={project} key={id}>
-      <div className="flex flex-col sm:flex-row gap-2 sm:gap-8 py-2">
-        <p className="flex-0 flex w-24 items-center gap-2 text-md text-gray-900 dark:text-white">
-          {data.title}
-        </p>
-        <p className="flex-1 sm:text-right text-sm text-gray-500 dark:text-gray-400">
-          {data.description}
-        </p>
-      </div>
-    </PrismicLink>
-  );
-};
+const Project: FC<{ title: KeyTextField; description: KeyTextField }> = ({
+  title,
+  description,
+}) => (
+  <div className="flex flex-col gap-2 py-2 sm:flex-row sm:gap-8">
+    <p className="flex-0 flex w-32 items-center gap-2 text-md text-gray-900 dark:text-white">
+      {title}
+    </p>
+    <p className="flex-1 text-sm text-gray-500 dark:text-gray-400 sm:text-right">
+      {description}
+    </p>
+  </div>
+);
 
 const Projects: FC<ProjectsProps> = ({ data, projects }) => (
   <Layout title={data.title} description={data.description}>
-    <div className="group mt-4">
-      {projects.map((item, index) => (
-        <Fragment key={index}>
-          {Boolean(index) && <Divider />}
-          <div className="transition-opacity group-hover:opacity-30 group-hover:hover:opacity-100">
-            <Project {...item} />
-          </div>
-        </Fragment>
-      ))}
+    <div className="flex flex-col gap-4">
+      <p className="text-sm text-gray-500 dark:text-gray-400">
+        {data.description}
+      </p>
+      <div className="group mt-4">
+        {projects.map((item, index) => (
+          <Fragment key={index}>
+            {Boolean(index) && <Divider />}
+            <div className="transition-opacity group-hover:opacity-30 group-hover:hover:opacity-100">
+              <PrismicLink document={item}>
+                <Project
+                  title={item.data.title}
+                  description={item.data.description}
+                />
+              </PrismicLink>
+            </div>
+          </Fragment>
+        ))}
+        {data.wip.map((item, index) => (
+          <Fragment key={index}>
+            {Boolean(index) && <Divider />}
+            <div className="transition-opacity group-hover:opacity-30 group-hover:hover:opacity-100">
+              {item.link.link_type === 'Any' ? (
+                <Project title={item.name} description={item.description} />
+              ) : (
+                <Link href={docResolver(item.link)} passHref>
+                  <a
+                    href={docResolver(item.link)}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Project title={item.name} description={item.description} />
+                  </a>
+                </Link>
+              )}
+            </div>
+          </Fragment>
+        ))}
+      </div>
     </div>
   </Layout>
 );

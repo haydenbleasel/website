f4022eb65ecaa07f15fc900808bf08bb08e2d6a6
Hayden Bleasel <hello@haydenbleasel.com>
2022-06-11 20:07:56 +1000

    Add redirects and fix sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,5 @@
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
   generateRobotsTxt: true,
+  exclude: ['/slice-simulator'],
 };
--- next.config.js
+++ next.config.js
@@ -29,6 +29,45 @@ const nextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: '/work/presumi',
+        destination: '/blog/presumi',
+        permanent: true,
+      },
+      {
+        source: '/thoughts/presumi',
+        destination: '/blog/presumi',
+        permanent: true,
+      },
+      {
+        source: '/presumi',
+        destination: '/blog/presumi',
+        permanent: true,
+      },
+      {
+        source: '/awards',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/contact',
+        destination: 'https://twitter.com/haydenbleasel',
+        permanent: true,
+      },
+      {
+        source: '/blog/jellypepper',
+        destination: '/work/jellypepper',
+        permanent: true,
+      },
+      {
+        source: '/journal/how-to-growth-hack-your-resume',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 const pwaConfig = {

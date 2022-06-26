020c72c1f63a84d29902fa169d7cf74c47c07068
Hayden Bleasel <hello@haydenbleasel.com>
2022-06-26 19:44:50 +1000

    Improve loading placeholder on Dribbble slider

--- slices/DribbbleSlider/index.tsx
+++ slices/DribbbleSlider/index.tsx
@@ -69,20 +69,20 @@ const Shot: FC<{ shot: number }> = ({ shot }) => {
         className="group relative flex w-full max-w-[400px] flex-shrink-0 flex-grow-0 flex-col"
       >
         <div className="flex flex-col overflow-hidden rounded-md bg-white shadow-md transition-all group-hover:shadow-lg dark:bg-gray-800">
-          {data ? (
-            <Image
-              src={data.image}
-              width={400}
-              height={300}
-              layout="responsive"
-              quality={100}
-              alt=""
-            />
-          ) : (
-            <div className="aspect-[4/3]">
-              <Placeholder className="h-full w-full" />
-            </div>
-          )}
+          <div className="relative aspect-[4/3] w-full">
+            <Placeholder className="absolute z-0 h-full w-full" />
+
+            {data && (
+              <Image
+                src={data.image}
+                width={400}
+                height={300}
+                layout="responsive"
+                quality={100}
+                alt=""
+              />
+            )}
+          </div>
           <div className="flex flex-col gap-1 border-t border-gray-100 p-4 dark:border-gray-700">
             <p className="m-0 text-lg font-semibold text-gray-900 line-clamp-1 dark:text-white">
               {data?.title ?? 'Loading'}

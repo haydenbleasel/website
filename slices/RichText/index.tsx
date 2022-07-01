71999258220cb831c1df065a5f816afed3eed92e
Hayden Bleasel <hello@haydenbleasel.com>
2022-07-01 17:32:11 +1000

    Remove paragraph indentation

--- slices/RichText/index.tsx
+++ slices/RichText/index.tsx
@@ -1,20 +1,7 @@
-import type { JSXMapSerializer, SliceComponentProps } from '@prismicio/react';
+import type { SliceComponentProps } from '@prismicio/react';
 import { PrismicRichText } from '@prismicio/react';
 import type { RichTextField } from '@prismicio/types';
 import type { FC } from 'react';
-import richTextComponents from '../../components/richTextComponents';
-
-const blogComponents: JSXMapSerializer = {
-  ...richTextComponents,
-  paragraph: ({ children, key }) => (
-    <p
-      key={key}
-      className="mb-4 indent-8 text-md font-normal text-gray-900 dark:text-white"
-    >
-      {children}
-    </p>
-  ),
-};
 
 const RichText: FC<
   SliceComponentProps<{
@@ -23,13 +10,6 @@ const RichText: FC<
       content: RichTextField;
     };
   }>
-> = ({ slice }) => (
-  <div>
-    <PrismicRichText
-      field={slice.primary.content}
-      components={blogComponents}
-    />
-  </div>
-);
+> = ({ slice }) => <PrismicRichText field={slice.primary.content} />;
 
 export default RichText;

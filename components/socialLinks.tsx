2778bfb753d2fcb0565910a50c2c6e6b0bde54fd
Hayden Bleasel <hello@haydenbleasel.com>
2022-07-09 20:56:57 +1000

    Add tooltips to social links

--- components/socialLinks.tsx
+++ components/socialLinks.tsx
@@ -1,31 +1,45 @@
 import Image from 'next/future/image';
 import Link from 'next/link';
 import type { FC } from 'react';
+import * as Tooltip from '@radix-ui/react-tooltip';
 import { social } from '../utils/social';
 
 const SocialLinks: FC = () => (
   <div className="-ml-3 flex flex-wrap">
     {social.map((platform, index) => (
-      <Link
-        href={platform.url}
-        key={platform.id}
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label={platform.name}
-        className="animate-enter p-4 opacity-0"
-        style={{ animationDelay: `${(index + 2) * 100}ms` }}
-      >
-        <Image
-          src={`/social/${platform.id}.svg`}
-          width={18}
-          height={18}
-          quality={100}
-          alt={platform.name}
-          className={`flex transition-transform hover:-translate-y-1 ${
-            platform.invertDark ? 'dark:brightness-0 dark:invert' : ''
-          }`}
-        />
-      </Link>
+      <Tooltip.Root key={platform.id}>
+        <Tooltip.Trigger className="flex animate-enter opacity-0">
+          <Link
+            href={platform.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label={platform.name}
+            className="p-4 transition-transform hover:-translate-y-1"
+            style={{ animationDelay: `${(index + 2) * 100}ms` }}
+          >
+            <Image
+              src={`/social/${platform.id}.svg`}
+              width={20}
+              height={20}
+              quality={100}
+              alt={platform.name}
+              className={`flex ${
+                platform.invertDark ? 'dark:brightness-0 dark:invert' : ''
+              }`}
+            />
+          </Link>
+        </Tooltip.Trigger>
+        <Tooltip.Content
+          side="top"
+          align="center"
+          sideOffset={0}
+          alignOffset={0}
+          className="rounded-md bg-gray-900/90 py-1 px-2 text-sm text-white shadow-lg backdrop-blur-sm"
+        >
+          {platform.name}
+          <Tooltip.Arrow className="fill-gray-900/90" />
+        </Tooltip.Content>
+      </Tooltip.Root>
     ))}
   </div>
 );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import Head from 'next/head';
 import { Toaster } from 'react-hot-toast';
 import { PrismicProvider } from '@prismicio/react';
 import { SocialProfileJsonLd } from 'next-seo';
+import * as Tooltip from '@radix-ui/react-tooltip';
 import { client, linkResolver } from '../utils/prismic';
 import '../styles/globals.css';
 import CommandBar from '../components/commandbar';
@@ -18,54 +19,59 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
   useAnalytics();
 
   return (
-    <CommandBar>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+    <Tooltip.Provider delayDuration={0}>
+      <CommandBar>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+          />
+
+          <meta name="application-name" content="Hayden Bleasel" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="Hayden Bleasel" />
+
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="msapplication-TileColor" content="#F5F5F9" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#F5F5F9" />
+          <link rel="manifest" href="/manifest.json" />
+
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" href="/favicon.png" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#F5F5F9" />
+        </Head>
+        <SocialProfileJsonLd
+          type="Person"
+          name="Hayden Bleasel"
+          url={process.env.NEXT_PUBLIC_SITE_URL ?? ''}
+          sameAs={Object.values(social).map(({ url }) => url)}
         />
-
-        <meta name="application-name" content="Hayden Bleasel" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="Hayden Bleasel" />
-
-        <meta name="format-detection" content="telephone=no" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="msapplication-TileColor" content="#F5F5F9" />
-        <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content="#F5F5F9" />
-        <link rel="manifest" href="/manifest.json" />
-
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" href="/favicon.png" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#F5F5F9" />
-      </Head>
-      <SocialProfileJsonLd
-        type="Person"
-        name="Hayden Bleasel"
-        url={process.env.NEXT_PUBLIC_SITE_URL ?? ''}
-        sameAs={Object.values(social).map(({ url }) => url)}
-      />
-      <PrismicProvider
-        linkResolver={linkResolver}
-        internalLinkComponent={InternalLinkComponent}
-        externalLinkComponent={ExternalLinkComponent}
-        client={client}
-        richTextComponents={richTextComponents}
-      >
-        <Component {...pageProps} />
-      </PrismicProvider>
-      <Menu />
-      <Toaster
-        containerClassName="print:hidden"
-        toastOptions={{
-          duration: 5000,
-          position: 'bottom-right',
-        }}
-      />
-    </CommandBar>
+        <PrismicProvider
+          linkResolver={linkResolver}
+          internalLinkComponent={InternalLinkComponent}
+          externalLinkComponent={ExternalLinkComponent}
+          client={client}
+          richTextComponents={richTextComponents}
+        >
+          <Component {...pageProps} />
+        </PrismicProvider>
+        <Menu />
+        <Toaster
+          containerClassName="print:hidden"
+          toastOptions={{
+            duration: 5000,
+            position: 'bottom-right',
+          }}
+        />
+      </CommandBar>
+    </Tooltip.Provider>
   );
 };
 

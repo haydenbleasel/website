fe98e57a9554b1937a1d12fda9ad444ed374c9e2
Hayden Bleasel <hello@haydenbleasel.com>
2022-07-09 21:14:36 +1000

    Let folks know they can use ⌘K to open the menu

--- components/menu.tsx
+++ components/menu.tsx
@@ -1,24 +1,33 @@
 import { useKBar } from 'kbar';
-import type { FC, KeyboardEventHandler } from 'react';
+import type { FC, KeyboardEventHandler, MouseEventHandler } from 'react';
+import { useState } from 'react';
 import { Menu as MenuList } from 'react-feather';
 import toast from 'react-hot-toast';
 
 const Menu: FC = () => {
   const kbar = useKBar();
+  const [clickNotified, setClickNotified] = useState(false);
 
   const keyDownHandler: KeyboardEventHandler<HTMLDivElement> = (event) => {
     if (event.code === 'Space') {
       event.preventDefault();
       event.stopPropagation();
       kbar.query.toggle();
-      toast('You can also press ⌘K to open the menu!');
     }
+  };
+
+  const clickHandler: MouseEventHandler<HTMLDivElement> = () => {
+    if (!clickNotified) {
+      toast('You can also press ⌘K to open the menu!');
+      setClickNotified(true);
+    }
+    kbar.query.toggle();
   };
 
   return (
     <div
       className="fixed top-0 right-0 z-20 p-4 text-gray-500 dark:text-gray-400 print:hidden"
-      onClick={() => kbar.query.toggle()}
+      onClick={clickHandler}
       onKeyDown={keyDownHandler}
       role="button"
       tabIndex={0}

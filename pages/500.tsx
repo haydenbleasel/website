107dc3e048d3206a8234cbfd6065c2f87d51ad95
Hayden Bleasel <hello@haydenbleasel.com>
2022-07-24 17:59:31 -0400

    Create custom 404 and 500 pages

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,22 +1,28 @@
 import { PrismicLink } from '@prismicio/react';
 import Image from 'next/future/image';
 import type { FC } from 'react';
+import { social } from '../utils/social';
 
-const NotFound: FC = () => (
+const ServerError: FC = () => (
   <div className="flex h-screen w-screen items-center justify-center">
     <div className="prose max-w-none text-center">
       <Image src="/images/bonk.webp" alt="Bonk" width={498} height={371} />
       <h1 className="m-0">
-        404:{' '}
-        <span className="text-gray-500 dark:text-gray-400">Page not found</span>
+        500:{' '}
+        <span className="text-gray-500 dark:text-gray-400">
+          Internal server error
+        </span>
       </h1>
       <p>
-        <PrismicLink className="font-medium" href="/">
-          Take me home, country roads
+        <PrismicLink
+          className="font-medium"
+          href={social.find(({ id }) => id === 'twitter')?.url}
+        >
+          Tell me my website sucks on Twitter
         </PrismicLink>
       </p>
     </div>
   </div>
 );
 
-export default NotFound;
+export default ServerError;

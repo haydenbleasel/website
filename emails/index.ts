479214890b30f282ffa21517a4cd515530d8971a
Hayden Bleasel <hello@haydenbleasel.com>
2022-08-07 09:33:41 -0400

    Add secure flag

--- emails/index.ts
+++ emails/index.ts
@@ -4,6 +4,7 @@ import { buildSendMail } from 'mailing-core';
 const transport = nodemailer.createTransport({
   host: 'smtp.sendgrid.net',
   port: 465,
+  secure: true,
   auth: {
     user: 'apikey',
     pass: process.env.SENDGRID_API_KEY,

4aa8e1af207230caa5fabb61f6382f6224d573ed
Hayden Bleasel <hello@haydenbleasel.com>
2022-08-18 14:26:10 -0400

    Update Mailing theme, remove unused component

--- emails/components/theme.ts
+++ emails/components/theme.ts
@@ -1,7 +1,15 @@
+import resolveConfig from 'tailwindcss/resolveConfig';
+import type { RequiredConfig } from 'tailwindcss/types/config';
+import type { DefaultColors } from 'tailwindcss/types/generated/colors';
+import tailwindConfig from '../../tailwind.config';
+
+const fullConfig = resolveConfig(tailwindConfig as RequiredConfig);
+const colors = fullConfig.theme?.colors as unknown as DefaultColors;
+
 // Colors
-export const black = '#000';
-export const grayDark = '#888';
-export const grayLight = '#f5f5f5';
+export const black = colors.neutral[900];
+export const grayDark = colors.neutral[500];
+export const grayLight = colors.neutral[100];
 
 // Typography
 export const textSm = 14;

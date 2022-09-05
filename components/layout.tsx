c2745de4ee65c09aa45ba554dab40cdf371e01d6
Hayden Bleasel <hello@haydenbleasel.com>
2022-09-04 22:48:48 -0400

    Add gamepad support (#61)
    * WIP Gamepad Support
    
    * Finish adding gamepad support
    
    * Workaround Typescript issue
    
    * Fix style properly

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,11 +1,14 @@
 import { PrismicLink } from '@prismicio/react';
 import type { ImageField } from '@prismicio/types';
+import { useKBar } from 'kbar';
 import type { NextSeoProps } from 'next-seo';
 import { NextSeo } from 'next-seo';
 import type { OpenGraphMedia } from 'next-seo/lib/types';
 import { useRouter } from 'next/router';
 import type { FC, ReactNode } from 'react';
+import { useEffect } from 'react';
 import { ArrowLeft } from 'react-feather';
+import useGamepadController from '../hooks/useGamepadController';
 import StickyTitle from './stickyTitle';
 
 export type LayoutProps = {
@@ -42,6 +45,42 @@ const getPreviousPage = (path: string) => {
   };
 };
 
+const getActiveKBarIndex = () => {
+  const activeElement = document.querySelector(
+    '#kbar-listbox > [aria-selected="true"]'
+  );
+  if (!activeElement) {
+    return undefined;
+  }
+  const id = activeElement.getAttribute('id');
+  const index = id?.split('-').at(-1);
+
+  if (!index) {
+    return undefined;
+  }
+
+  const parsedIndex = Number(index);
+
+  return parsedIndex;
+};
+
+const getLastKBarIndex = () => {
+  const activeElement = document.querySelectorAll('#kbar-listbox > div');
+
+  const lastElement = activeElement[activeElement.length - 1];
+
+  const id = lastElement.getAttribute('id');
+  const index = id?.split('-').at(-1);
+
+  if (!index) {
+    return undefined;
+  }
+
+  const parsedIndex = Number(index);
+
+  return parsedIndex;
+};
+
 const Layout: FC<LayoutProps> = ({
   title,
   description,
@@ -53,9 +92,100 @@ const Layout: FC<LayoutProps> = ({
   noTitle = false,
   ...props
 }) => {
-  const { asPath } = useRouter();
-  const siteUrl = new URL(asPath, process.env.NEXT_PUBLIC_SITE_URL).href;
-  const previousPage = getPreviousPage(asPath);
+  const router = useRouter();
+  const siteUrl = new URL(router.asPath, process.env.NEXT_PUBLIC_SITE_URL).href;
+  const previousPage = getPreviousPage(router.asPath);
+  const gamepadState = useGamepadController();
+  const kbar = useKBar();
+
+  useEffect(() => {
+    if (gamepadState.options) {
+      router.reload();
+    }
+  }, [gamepadState.options, router]);
+
+  useEffect(() => {
+    if (gamepadState.y) {
+      kbar.query.toggle();
+    }
+  }, [gamepadState.y, kbar.query]);
+
+  useEffect(() => {
+    if (typeof window === 'undefined' || !gamepadState.down) {
+      return;
+    }
+
+    const isKBarOpen = document.body.style.overflowY === 'hidden';
+    if (isKBarOpen) {
+      const activeIndex = getActiveKBarIndex();
+      const lastIndex = getLastKBarIndex();
+      if (
+        activeIndex !== undefined &&
+        lastIndex !== undefined &&
+        activeIndex < lastIndex
+      ) {
+        kbar.query.setActiveIndex(activeIndex + 1);
+      }
+    } else {
+      window.scrollTo({ top: window.scrollY + window.innerHeight });
+    }
+  }, [gamepadState.down, kbar.query]);
+
+  useEffect(() => {
+    if (typeof window === 'undefined' || !gamepadState.a) {
+      return;
+    }
+
+    const isKBarOpen = document.body.style.overflowY === 'hidden';
+    if (!isKBarOpen) {
+      return;
+    }
+
+    const activeElement = document.querySelector<HTMLDivElement>(
+      '#kbar-listbox > [aria-selected="true"]'
+    );
+
+    if (!activeElement) {
+      return;
+    }
+
+    activeElement.click();
+  }, [gamepadState.a]);
+
+  useEffect(() => {
+    if (typeof window === 'undefined' || !gamepadState.b) {
+      return;
+    }
+
+    const isKBarOpen = document.body.style.overflowY === 'hidden';
+    if (!isKBarOpen) {
+      return;
+    }
+
+    kbar.query.setCurrentRootAction(null);
+  }, [gamepadState.b, kbar.query]);
+
+  useEffect(() => {
+    if (typeof window === 'undefined' || !gamepadState.up) {
+      return;
+    }
+
+    const isKBarOpen = document.body.style.overflowY === 'hidden';
+    if (isKBarOpen) {
+      const activeIndex = getActiveKBarIndex();
+      if (activeIndex !== undefined && activeIndex > 0) {
+        kbar.query.setActiveIndex(activeIndex - 1);
+      }
+    } else {
+      window.scrollTo({ top: window.scrollY - window.innerHeight });
+    }
+  }, [gamepadState.up, kbar.query]);
+
+  useEffect(() => {
+    if (gamepadState.share && typeof window !== 'undefined') {
+      router.back();
+    }
+  }, [gamepadState.share, router]);
 
   if (!title || !description) {
     throw new Error(

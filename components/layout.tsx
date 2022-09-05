258283a8aeda420a91b93426cf87868b05503de1
Hayden Bleasel <hello@haydenbleasel.com>
2022-09-04 21:43:38 -0400

    Remove dynamic imports for high-level items

--- components/layout.tsx
+++ components/layout.tsx
@@ -5,12 +5,7 @@ import { NextSeo } from 'next-seo';
 import type { OpenGraphMedia } from 'next-seo/lib/types';
 import { useRouter } from 'next/router';
 import type { FC, ReactNode } from 'react';
-import { Suspense } from 'react';
 import { ArrowLeft } from 'react-feather';
-import dynamic from 'next/dynamic';
-import useAnalytics from '../hooks/useAnalytics';
-import useNetworkMonitor from '../hooks/useNetworkMonitor';
-import useThemeListener from '../hooks/useThemeListener';
 import StickyTitle from './stickyTitle';
 
 export type LayoutProps = {
@@ -61,33 +56,6 @@ const Layout: FC<LayoutProps> = ({
   const { asPath } = useRouter();
   const siteUrl = new URL(asPath, process.env.NEXT_PUBLIC_SITE_URL).href;
   const previousPage = getPreviousPage(asPath);
-  const Activity = dynamic(
-    async () =>
-      import(
-        /* webpackChunkName: "activity" */
-        './activity'
-      ),
-    { ssr: false, suspense: true }
-  );
-  const Menu = dynamic(
-    async () =>
-      import(
-        /* webpackChunkName: "menu" */
-        './menu'
-      ),
-    { ssr: false, suspense: true }
-  );
-  const CommandBar = dynamic(
-    async () =>
-      import(
-        /* webpackChunkName: "CommandBar" */
-        './commandbar'
-      ),
-    { ssr: false, suspense: true }
-  );
-  useAnalytics();
-  useNetworkMonitor();
-  useThemeListener();
 
   if (!title || !description) {
     throw new Error(
@@ -118,73 +86,66 @@ const Layout: FC<LayoutProps> = ({
   }
 
   return (
-    <>
-      <div className="py-12 print:py-4 sm:gap-24 sm:py-48">
-        <NextSeo
-          title={title}
-          titleTemplate="%s — Hayden Bleasel"
-          description={description}
-          canonical={siteUrl}
-          openGraph={{
-            url: siteUrl,
-            title,
-            description,
-            images,
-            site_name: 'Hayden Bleasel',
-            type: 'profile',
-            profile: {
-              firstName: 'Hayden',
-              lastName: 'Bleasel',
-              username: 'haydenbleasel',
-              gender: 'male',
-            },
-          }}
-          twitter={{
-            handle: '@haydenbleasel',
-            site: '@haydenbleasel',
-            cardType: 'summary_large_image',
-          }}
-          {...props}
-        />
-        <StickyTitle noSticky={noSticky} noTitle={noTitle}>
-          {title}
-        </StickyTitle>
-        <div className="container prose mx-auto px-4 dark:prose-invert">
-          {(caption || subtitle) && (
-            <div className="mb-8 flex flex-col gap-1">
-              {subtitle && (
-                <p className="m-0 animate-enter opacity-0 animation-delay-100">
-                  {subtitle}
-                </p>
-              )}
-              {caption && (
-                <p className="m-0 animate-enter text-sm text-neutral-500 opacity-0 animation-delay-100 dark:text-neutral-400">
-                  {caption}
-                </p>
-              )}
-            </div>
-          )}
-          {children}
-        </div>
-        {previousPage && (
-          <div className="fixed top-0 left-0 z-20 print:hidden">
-            <PrismicLink href={previousPage.href}>
-              <span className="flex items-center gap-1 p-4 text-neutral-500 dark:text-neutral-400">
-                <ArrowLeft size={16} />
-                <span className="text-sm leading-[1rem] text-neutral-500 dark:text-neutral-400">
-                  {previousPage.label}
-                </span>
-              </span>
-            </PrismicLink>
+    <div className="py-12 print:py-4 sm:gap-24 sm:py-48">
+      <NextSeo
+        title={title}
+        titleTemplate="%s — Hayden Bleasel"
+        description={description}
+        canonical={siteUrl}
+        openGraph={{
+          url: siteUrl,
+          title,
+          description,
+          images,
+          site_name: 'Hayden Bleasel',
+          type: 'profile',
+          profile: {
+            firstName: 'Hayden',
+            lastName: 'Bleasel',
+            username: 'haydenbleasel',
+            gender: 'male',
+          },
+        }}
+        twitter={{
+          handle: '@haydenbleasel',
+          site: '@haydenbleasel',
+          cardType: 'summary_large_image',
+        }}
+        {...props}
+      />
+      <StickyTitle noSticky={noSticky} noTitle={noTitle}>
+        {title}
+      </StickyTitle>
+      <div className="container prose mx-auto px-4 dark:prose-invert">
+        {(caption || subtitle) && (
+          <div className="mb-8 flex flex-col gap-1">
+            {subtitle && (
+              <p className="m-0 animate-enter opacity-0 animation-delay-100">
+                {subtitle}
+              </p>
+            )}
+            {caption && (
+              <p className="m-0 animate-enter text-sm text-neutral-500 opacity-0 animation-delay-100 dark:text-neutral-400">
+                {caption}
+              </p>
+            )}
           </div>
         )}
+        {children}
       </div>
-      <Suspense>
-        <Activity />
-        <Menu />
-        <CommandBar />
-      </Suspense>
-    </>
+      {previousPage && (
+        <div className="fixed top-0 left-0 z-20 print:hidden">
+          <PrismicLink href={previousPage.href}>
+            <span className="flex items-center gap-1 p-4 text-neutral-500 dark:text-neutral-400">
+              <ArrowLeft size={16} />
+              <span className="text-sm leading-[1rem] text-neutral-500 dark:text-neutral-400">
+                {previousPage.label}
+              </span>
+            </span>
+          </PrismicLink>
+        </div>
+      )}
+    </div>
   );
 };
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,67 +14,82 @@ import '../styles/globals.css';
 import ExternalLinkComponent from '../components/externalLink';
 import richTextComponents from '../components/richTextComponents';
 import { social } from '../utils/social';
+import Menu from '../components/menu';
+import CommandBar from '../components/commandbar';
+import Activity from '../components/activity';
+import useAnalytics from '../hooks/useAnalytics';
+import useNetworkMonitor from '../hooks/useNetworkMonitor';
+import useThemeListener from '../hooks/useThemeListener';
 
 const InternalLinkComponent = (props: LinkProps) => <Link {...props} />;
 
-const App: FC<AppProps> = ({ Component, pageProps }) => (
-  <>
-    <Head>
-      <meta charSet="utf-8" />
-      <meta
-        name="viewport"
-        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+  useAnalytics();
+  useNetworkMonitor();
+  useThemeListener();
+
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+        />
+
+        <meta name="application-name" content="Hayden Bleasel" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Hayden Bleasel" />
+
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileColor" content="#F5F5F9" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#F5F5F9" />
+        <link rel="manifest" href="/manifest.json" />
+
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#F5F5F9" />
+      </Head>
+      <SocialProfileJsonLd
+        type="Person"
+        name="Hayden Bleasel"
+        url={process.env.NEXT_PUBLIC_SITE_URL ?? ''}
+        sameAs={Object.values(social).map(({ url }) => url)}
       />
-
-      <meta name="application-name" content="Hayden Bleasel" />
-      <meta name="apple-mobile-web-app-capable" content="yes" />
-      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-      <meta name="apple-mobile-web-app-title" content="Hayden Bleasel" />
-
-      <meta name="format-detection" content="telephone=no" />
-      <meta name="mobile-web-app-capable" content="yes" />
-      <meta name="msapplication-TileColor" content="#F5F5F9" />
-      <meta name="msapplication-tap-highlight" content="no" />
-      <meta name="theme-color" content="#F5F5F9" />
-      <link rel="manifest" href="/manifest.json" />
-
-      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" href="/favicon.png" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#F5F5F9" />
-    </Head>
-    <SocialProfileJsonLd
-      type="Person"
-      name="Hayden Bleasel"
-      url={process.env.NEXT_PUBLIC_SITE_URL ?? ''}
-      sameAs={Object.values(social).map(({ url }) => url)}
-    />
-    <TooltipProvider delayDuration={0}>
-      <KBarProvider>
-        <PrismicProvider
-          linkResolver={linkResolver}
-          internalLinkComponent={InternalLinkComponent}
-          externalLinkComponent={ExternalLinkComponent}
-          client={createClient()}
-          richTextComponents={richTextComponents}
-        >
-          <PrismicPreview
-            repositoryName={process.env.NEXT_PUBLIC_PRISMIC_ENDPOINT ?? ''}
+      <TooltipProvider delayDuration={0}>
+        <KBarProvider>
+          <PrismicProvider
+            linkResolver={linkResolver}
+            internalLinkComponent={InternalLinkComponent}
+            externalLinkComponent={ExternalLinkComponent}
+            client={createClient()}
+            richTextComponents={richTextComponents}
           >
-            <Component {...pageProps} />
-          </PrismicPreview>
-        </PrismicProvider>
-      </KBarProvider>
-    </TooltipProvider>
-    <Toaster
-      containerClassName="print:hidden"
-      toastOptions={{
-        duration: 5000,
-        position: 'bottom-right',
-        className:
-          '!bg-neutral-900/90 !backdrop-blur-md !text-white !rounded-sm',
-      }}
-    />
-  </>
-);
+            <PrismicPreview
+              repositoryName={process.env.NEXT_PUBLIC_PRISMIC_ENDPOINT ?? ''}
+            >
+              <Component {...pageProps} />
+            </PrismicPreview>
+            <Menu />
+            <CommandBar />
+            <Activity />
+          </PrismicProvider>
+        </KBarProvider>
+      </TooltipProvider>
+      <Toaster
+        containerClassName="print:hidden"
+        toastOptions={{
+          duration: 5000,
+          position: 'bottom-right',
+          className:
+            '!bg-neutral-900/90 !backdrop-blur-md !text-white !rounded-sm',
+        }}
+      />
+    </>
+  );
+};
 
 export default App;

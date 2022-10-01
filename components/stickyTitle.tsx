557cab95d80b858c923b970cb1018e1c46bc2bec
Hayden Bleasel <hello@haydenbleasel.com>
2022-10-01 17:38:55 -0400

    Swap out custom code for useSticky

--- components/stickyTitle.tsx
+++ components/stickyTitle.tsx
@@ -1,6 +1,6 @@
 import type { FC, ReactNode } from 'react';
 import { useRef } from 'react';
-import { useIntersectionObserver } from '@react-hookz/web';
+import useSticky from '@haydenbleasel/use-sticky';
 
 type StickyTitleProps = {
   noSticky: boolean;
@@ -9,22 +9,17 @@ type StickyTitleProps = {
 };
 
 const StickyTitle: FC<StickyTitleProps> = ({ noSticky, noTitle, children }) => {
-  const intersectionRef = useRef(null);
-  const intersection = useIntersectionObserver(intersectionRef, {
-    root: null,
-    rootMargin: '0px',
-    threshold: [1],
-  });
-  const isSticky = intersection ? !intersection.isIntersecting : false;
+  const ref = useRef(null);
+  const isSticky = useSticky(ref);
 
   return (
     <div
-      className={`top-[-1px] z-10 border-b bg-transparent pt-12 print:hidden sm:pt-0 ${
+      className={`z-10 border-b bg-transparent pt-12 print:hidden sm:pt-0 ${
         isSticky && !noSticky
           ? 'border-neutral-200 bg-neutral-100/80 backdrop-blur-sm transition-all dark:border-neutral-700 dark:bg-neutral-900/80'
           : 'border-transparent'
       } ${noSticky ? '' : 'sticky'}`}
-      ref={intersectionRef}
+      ref={ref}
     >
       {!noTitle && (
         <div className="container prose mx-auto grid max-w-[42rem] py-2 px-4 dark:prose-invert">

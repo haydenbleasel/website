7c9bc5b3ec5f2411ba9e1934aa62e7898af5165f
Hayden Bleasel <hello@haydenbleasel.com>
2022-11-29 20:33:19 -0500

    Remove scaffolding, add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.tsx', './components/**/*.tsx', './content/**/*.mdx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

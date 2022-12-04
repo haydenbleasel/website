23626ed4964b7394340c393f8e2376bd478a8492
Hayden Bleasel <hello@haydenbleasel.com>
2022-12-04 16:05:43 -0500

    Replace Inter with Lausanne

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import type { FC, ReactNode } from 'react';
 import clsx from 'clsx';
 import '@/styles/globals.css';
-import { ibmPlexMono, inter } from '@/lib/fonts';
 import { Analytics } from './components/analytics';
+import { ibmPlexMono, lausanne } from '@/lib/fonts';
 
 const RootLayout: FC<{ children: ReactNode }> = ({ children }) => (
   <html
     lang="en"
     className={clsx(
       ibmPlexMono.variable,
-      inter.variable,
+      lausanne.variable,
       'bg-neutral-50 font-sans font-light'
     )}
   >
--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,5 +1,5 @@
 import { IBM_Plex_Mono as createIBMPlex } from '@next/font/google';
-import { Inter as createInter } from '@next/font/google';
+import localFont from '@next/font/local';
 
 export const ibmPlexMono = createIBMPlex({
   weight: '400',
@@ -7,7 +7,38 @@ export const ibmPlexMono = createIBMPlex({
   subsets: ['latin'],
 });
 
-export const inter = createInter({
-  variable: '--font-inter',
-  subsets: ['latin'],
+export const lausanne = localFont({
+  variable: '--font-lausanne',
+  src: [
+    {
+      path: '../../public/fonts/TWK-Lausanne-300.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/TWK-Lausanne-300Italic.woff2',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/TWK-Lausanne-400.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/TWK-Lausanne-400Italic.woff2',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/TWK-Lausanne-500.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/TWK-Lausanne-500Italic.woff2',
+      weight: '500',
+      style: 'italic',
+    },
+  ],
 });

469a89bbe60511e45e0b0dc5c8964683fd1c9612
Hayden Bleasel <hello@haydenbleasel.com>
2022-12-04 23:18:44 -0500

    Bring over Contact form handler and Vercel OG generator

---
+++ pages/api/contact.ts
@@ -0,0 +1,87 @@
+import { format } from 'date-fns';
+import domains from 'disposable-email-domains';
+import type { NextApiHandler } from 'next';
+import type { ReactElement } from 'react';
+import sendMail from '../../../emails';
+import template from '../../../emails/TextEmail';
+import parseBody from '@/lib/parseBody';
+import parseError from '@/lib/parseError';
+
+type ContactResponse = {
+  error?: string;
+  message?: string;
+};
+
+export const config = {
+  runtime: 'nodejs',
+};
+
+const handler: NextApiHandler<ContactResponse> = async (req, res) => {
+  const { name, email, message } = parseBody<{
+    name?: string;
+    email?: string;
+    message?: string;
+  }>(req);
+
+  if (
+    req.headers.authorization !==
+    `Bearer ${process.env.NEXT_PUBLIC_API_PASSPHRASE ?? ''}`
+  ) {
+    res.status(401).json({ error: 'Unauthorized' });
+    return;
+  }
+
+  if (!name) {
+    res.status(400).json({ error: 'No name provided' });
+    return;
+  }
+
+  if (!email) {
+    res.status(400).json({ error: 'Missing email field' });
+    return;
+  }
+
+  if (!message) {
+    res.status(400).json({ error: 'Missing message field' });
+    return;
+  }
+
+  const domain = email.split('@')[1];
+
+  if (domains.includes(domain)) {
+    res.status(400).json({
+      error:
+        "Sorry, I don't accept disposable email addresses. Please use a different email address.",
+    });
+    return;
+  }
+
+  const ip = req.headers['x-forwarded-for'] as string | undefined;
+  const now = new Date();
+
+  try {
+    await sendMail({
+      subject: 'Incoming message',
+      to: process.env.EMAIL_ADDRESS,
+      replyTo: email,
+      component: template({
+        name,
+        message,
+        items: [
+          `Date: ${format(now, 'MMMM do, yyyy')} at ${format(now, 'h:mm a')}`,
+          `Email: ${email}`,
+          `IP: ${ip ?? req.socket.remoteAddress ?? 'Unknown'}`,
+          `Device: ${req.headers['user-agent'] ?? 'Unknown'}`,
+        ],
+      }) as ReactElement,
+    });
+
+    res.status(200).json({ message: 'Email sent' });
+  } catch (error) {
+    const errorMessage = parseError(error);
+
+    res.status(500).json({ error: errorMessage });
+  }
+};
+
+export default handler;
---
+++ pages/api/og/index.tsx
@@ -0,0 +1,91 @@
+/* eslint-disable @next/next/no-img-element */
+
+import { ImageResponse } from '@vercel/og';
+import type { NextRequest } from 'next/server';
+import parseError from '@/lib/parseError';
+
+export const config = {
+  runtime: 'experimental-edge',
+};
+
+const createString = (str: string | null, fallback: string): string => {
+  if (!str) {
+    return fallback;
+  }
+
+  if (str.length > 100) {
+    return `${str.slice(0, 100)}...`;
+  }
+
+  return str;
+};
+
+const handler = async (req: NextRequest): Promise<ImageResponse> => {
+  const { searchParams } = new URL(req.url);
+  const InterRegular = await fetch(
+    new URL('./Inter-Regular.otf', import.meta.url)
+  ).then(async (res) => res.arrayBuffer());
+  const InterBold = await fetch(
+    new URL('./Inter-Bold.otf', import.meta.url)
+  ).then(async (res) => res.arrayBuffer());
+
+  const avatarUrl = new URL(
+    'avatar.jpg',
+    process.env.NEXT_PUBLIC_SITE_URL ?? ''
+  ).href;
+  const title = createString(searchParams.get('title'), 'Hayden Bleasel');
+  const subtitle = createString(
+    searchParams.get('subtitle'),
+    'Digital Product Designer and Software Engineer'
+  );
+  const path = createString(searchParams.get('path'), '/');
+  const { hostname } = new URL(process.env.NEXT_PUBLIC_SITE_URL ?? '');
+  const url = `${hostname}${path}`;
+
+  try {
+    return new ImageResponse(
+      (
+        <div tw="flex bg-gray-50 flex-1 w-full h-full justify-center flex-col py-12 px-32">
+          <img
+            width={64}
+            height={64}
+            src={avatarUrl}
+            tw="rounded-full w-16 h-16 mb-12 block"
+            alt=""
+          />
+          <p tw="text-5xl tracking-tight leading-[1.1] font-bold mt-0 mb-6 w-[70%] text-gray-900">
+            {title}
+          </p>
+          <p tw="text-2xl mt-0 mb-8 w-[70%] text-gray-600">{subtitle}</p>
+          <p tw="text-md m-0 text-gray-500 w-[70%]">{url}</p>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+        fonts: [
+          {
+            name: 'Inter',
+            data: InterRegular,
+            weight: 400,
+            style: 'normal',
+          },
+          {
+            name: 'Inter',
+            data: InterBold,
+            weight: 700,
+            style: 'normal',
+          },
+        ],
+      }
+    );
+  } catch (error) {
+    const message = parseError(error);
+
+    return new Response(`Failed to generate the image: ${message}`, {
+      status: 500,
+    });
+  }
+};
+
+export default handler;

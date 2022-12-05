22a4dae77a2a0274a32dfa8cfd2a5a837998f213
Hayden Bleasel <hello@haydenbleasel.com>
2022-12-05 11:57:27 -0500

    Add offline notification

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,13 +1,31 @@
 'use client';
 import type { FC, ReactNode } from 'react';
+import { useEffect } from 'react';
 import { Provider as TooltipProvider } from '@radix-ui/react-tooltip';
+import { toast, Toaster } from 'react-hot-toast';
+import useOnline from '@haydenbleasel/use-online';
 
 type ProvidersProps = {
   children: ReactNode;
 };
 
-const Providers: FC<ProvidersProps> = ({ children }) => (
-  <TooltipProvider delayDuration={0}>{children}</TooltipProvider>
-);
+const Providers: FC<ProvidersProps> = ({ children }) => {
+  const isOnline = useOnline();
+
+  useEffect(() => {
+    if (!isOnline) {
+      toast.error(
+        'You have been disconnected from the internet. Certain features may not work until you reconnect.'
+      );
+    }
+  }, [isOnline]);
+
+  return (
+    <>
+      <TooltipProvider delayDuration={0}>{children}</TooltipProvider>
+      <Toaster position="bottom-right" />
+    </>
+  );
+};
 
 export default Providers;

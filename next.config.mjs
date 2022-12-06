3943c60a18f91a42da597c3c494a131383b3b6e0
Hayden Bleasel <hello@haydenbleasel.com>
2022-12-06 18:50:54 -0500

    Ignore TS issues in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { withContentlayer } from 'next-contentlayer';
 import { createSecureHeaders } from 'next-secure-headers';
 import withPWA from 'next-pwa';

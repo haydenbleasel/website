abc859c9332a15c86b45acc8e01ec57def2e35db
Hayden Bleasel <hello@haydenbleasel.com>
2022-12-05 22:57:08 -0500

    Misc fixes

--- app/press/page.tsx
+++ app/press/page.tsx
@@ -13,15 +13,16 @@ type FeatureProps = {
   date: string;
 };
 
-const Feature: FC<FeatureProps> = ({ name, source, link, date }) => (
-  <Link
-    href={link ?? ''}
-    className="flex flex-col gap-1 py-2 no-underline sm:flex-row sm:gap-8"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
+const FeatureInner: FC<Omit<FeatureProps, 'link'>> = ({
+  name,
+  source,
+  date,
+}) => (
+  <>
     <span className="flex-1 line-clamp-1">
-      <span className="flex-0 flex items-center gap-2">{name}</span>
+      <span className="flex-0 flex items-center gap-2 font-medium text-zinc-900 line-clamp-1 dark:text-white">
+        {name}
+      </span>
       <span className="flex-1 text-sm font-light text-zinc-500 line-clamp-1 dark:text-zinc-400">
         {source}
       </span>
@@ -29,8 +30,24 @@ const Feature: FC<FeatureProps> = ({ name, source, link, date }) => (
     <span className="flex-shrink-0 text-sm font-light text-zinc-500 sm:leading-[28px]">
       {formatDate(new Date(date))}
     </span>
-  </Link>
+  </>
 );
+
+const Feature: FC<FeatureProps> = ({ link, ...props }) =>
+  link ? (
+    <Link
+      href={link}
+      className="flex flex-col gap-1 py-2 no-underline sm:flex-row sm:gap-8"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <FeatureInner {...props} />
+    </Link>
+  ) : (
+    <div className="flex flex-col gap-1 py-2 no-underline sm:flex-row sm:gap-8">
+      <FeatureInner {...props} />
+    </div>
+  );
 
 const Press: FC = () => (
   <main className="flex flex-col gap-6 prose-h2:mb-2 prose-h2:mt-4 prose-p:m-0">

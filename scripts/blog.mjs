05ba41656408fadefd54424a72e1f832f6e6fd56
Hayden Bleasel <hello@haydenbleasel.com>
2022-12-08 11:46:54 -0500

    Blog figma autolayout (#79)
    * Scaffold blog creator script
    
    * Scaffold article using script
    
    * Fix typo
    
    * Finish Autolayout post
    
    * Update with GIF

---
+++ scripts/blog.mjs
@@ -0,0 +1,43 @@
+// eslint-disable-next-line import/no-nodejs-modules
+import fs from 'fs/promises';
+import inquirer from 'inquirer';
+
+const main = async () => {
+  console.log('Scaffolding a new blog post...');
+  try {
+    const { slug, title, description, date } = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'slug',
+        message: 'What is the slug of your blog post?',
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your blog post?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of your blog post?',
+      },
+      {
+        type: 'input',
+        name: 'date',
+        message: 'What is the date of your blog post? (MM-DD-YYYY)',
+      },
+    ]);
+
+    const content = `---
+title: ${title}
+description: ${description}
+date: ${date}
+---\n\n`;
+
+    await fs.writeFile(`./content/blog/${slug}.mdx`, content);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+main();

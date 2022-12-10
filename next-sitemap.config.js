9371aab55401dbf8c2298e6585925c02bc7217f5
Hayden Bleasel <hello@haydenbleasel.com>
2022-12-09 23:45:26 -0500

    Temporary fix for spread routes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next-sitemap').IConfig} */
 
+// Fix spread slug issues e.g.
+// http://localhost:3000/work/%2Fwork%2Fcorellium
+// http://localhost:3000/blog/%2Fblog%2Fpresumi
+
+const fixPath = (path) => {
+  return path
+    .replace('/work/%2Fwork%2F', '/work/')
+    .replace('/blog/%2Fblog%2F', '/blog/');
+};
+
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
   generateRobotsTxt: true,
+  transform: async (config, path) => {
+    return {
+      loc: fixPath(path),
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
 };

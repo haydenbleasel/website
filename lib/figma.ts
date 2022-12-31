2612afb55c09ef58127355c2618e7b1caaa9857c
Hayden Bleasel <hello@haydenbleasel.com>
2022-12-31 17:27:11 -0500

    Move Design and Code to Beskar

--- lib/figma.ts
+++ lib/figma.ts
@@ -22,24 +22,6 @@ type FigmaDocument = {
   ];
 };
 
-type FigmaProject = {
-  name: string;
-  files: {
-    key: string;
-    name: string;
-    thumbnail_url: string;
-    last_modified: string;
-    branches: [
-      {
-        key: string;
-        name: string;
-        thumbnail_url: string;
-        last_modified: string;
-      }
-    ];
-  }[];
-};
-
 const options = {
   headers: {
     // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -53,15 +35,4 @@ export const fetchFigmaFile = async (id: string): Promise<FigmaDocument> => {
   const file = (await response.json()) as FigmaDocument;
 
   return file;
-};
-
-export const fetchFigmaProject = async (id: string): Promise<FigmaProject> => {
-  const response = await fetch(
-    `https://api.figma.com/v1/projects/${id}/files`,
-    options
-  );
-
-  const project = (await response.json()) as FigmaProject;
-
-  return project;
 };

eebf1afb5bf50935effe08d0e5b8b79e39d61dec
Hayden Bleasel <hello@haydenbleasel.com>
2022-12-31 18:18:51 -0500

    Redirect playlists to Spotify

--- lib/navigation.ts
+++ lib/navigation.ts
@@ -1,16 +1,9 @@
-import {
-  Briefcase,
-  Headphones,
-  Home,
-  Lightbulb,
-  MessageCircle,
-} from 'lucide-react';
+import { Briefcase, Home, Lightbulb, MessageCircle } from 'lucide-react';
 
 const pages = [
   { name: 'Home', icon: Home, href: '/' },
   { name: 'Work', icon: Briefcase, href: '/work' },
   { name: 'Case Studies', icon: Lightbulb, href: '/case-studies' },
-  { name: 'Playlists', icon: Headphones, href: '/playlists' },
   { name: 'Contact', icon: MessageCircle, href: '/contact' },
 ];
 

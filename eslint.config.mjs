9bc155703447895bab3a6c82f356d5e6a09aeee6
Hayden Bleasel <hello@haydenbleasel.com>
2023-01-14 13:35:31 -0500

    Patch weird ESLint regression

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,9 @@
 import harmony from '@beskar-labs/harmony';
 
+// Fix weird ESLint regression
+harmony[0].languageOptions.globals.AudioWorkletGlobalScope =
+  harmony[0].languageOptions.globals['AudioWorkletGlobalScope '];
+
+delete harmony[0].languageOptions.globals['AudioWorkletGlobalScope '];
+
 export default harmony;

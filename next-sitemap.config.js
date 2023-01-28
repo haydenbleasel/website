5733876337a5e31f2343079fadab8576c0353136
Hayden Bleasel <hello@haydenbleasel.com>
2023-01-27 21:06:51 -0500

    Remove index sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,25 +1,6 @@
-// Fix spread slug issues e.g.
-// http://localhost:3000/work/%2Fwork%2Fcorellium
-// http://localhost:3000/blog/%2Fblog%2Fpresumi
-
-const fixPath = (path) => {
-  return path
-    .replace('/work/%2Fwork%2F', '/work/')
-    .replace('/blog/%2Fblog%2F', '/blog/')
-    .replace('/case-studies/%2Fcase-studies%2F', '/case-studies/');
-};
-
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
   generateRobotsTxt: true,
-  transform: async (config, path) => {
-    return {
-      loc: fixPath(path),
-      changefreq: config.changefreq,
-      priority: config.priority,
-      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
-      alternateRefs: config.alternateRefs ?? [],
-    };
-  },
+  generateIndexSitemap: false,
 };

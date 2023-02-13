4009ab291f04f430278eab72b80facdab65eb663
Hayden Bleasel <hello@haydenbleasel.com>
2023-02-13 01:16:55 -0500

    Cleanup files

--- lib/formatDate.ts
+++
@@ -1,8 +0,0 @@
-const formatDate = (date: Date): string =>
-  new Intl.DateTimeFormat('en-US', {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-  }).format(date);
-
-export default formatDate;

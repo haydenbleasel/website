548fc088fa887f7e391815f9c27b2cf69596e086
Hayden Bleasel <hello@haydenbleasel.com>
2023-04-05 17:57:58 -0400

    Add Jellypepper and Freelance clients lists

---
+++ data/freelance.ts
@@ -0,0 +1,10 @@
+import { sortByName } from '@/lib/utils';
+
+const freelanceClients = [
+  'ESLint',
+  'OpenJS Foundation',
+  'UTS',
+  'Tyro Payments',
+].sort(sortByName);
+
+export default freelanceClients;
---
+++ data/jellypepper.ts
@@ -0,0 +1,36 @@
+import { sortByName } from '@/lib/utils';
+
+const jellypepperClients = [
+  'AdvanCell',
+  'Airwallex',
+  'Audience Republic',
+  'Baraja',
+  'Brighte',
+  'Canva',
+  'Clipchamp',
+  'Elevio',
+  'Faethm',
+  'Flaunter',
+  'Flirtey',
+  'Futrli',
+  'Grow',
+  'Inventia',
+  'Kerbly',
+  'Notiv',
+  'Perlin',
+  'Pursuited',
+  'Resolution Collective',
+  'Rezi',
+  'Ribit',
+  'Shippit',
+  'Siesta Campers',
+  'Simply Wall St',
+  'Snug',
+  'Space Machines Company',
+  'Tank Stream Ventures',
+  'UpGuard',
+  'Zibbet',
+  'Zookal',
+].sort(sortByName);
+
+export default jellypepperClients;

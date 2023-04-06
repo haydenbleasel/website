6a1d77d58dac32048bf969cdef8a0d59ea6c75c8
Hayden Bleasel <hello@haydenbleasel.com>
2023-04-06 18:20:06 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,52 @@
+/* eslint-disable id-length */
 import type { Config } from 'tailwindcss';
+import typography from '@tailwindcss/typography';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 const config: Config = {
-  content: ['./app/**/*.{ts,tsx}'],
+  content: ['./{app,components}/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme: (str: string) => string) => ({
+        DEFAULT: {
+          css: {
+            maxWidth: '31.25rem',
+            '-webkit-font-smoothing': 'antialiased',
+            '-moz-osx-font-smoothing': 'grayscale',
+            textRendering: 'geometricPrecision',
+            color: theme('colors.neutral.950'),
+            p: {
+              lineHeight: '154%',
+            },
+            h1: {
+              color: theme('colors.neutral.950'),
+              margin: 0,
+              fontFamily: theme('fontFamily.serif'),
+              fontStyle: 'italic',
+              fontWeight: theme('fontWeight.normal'),
+            },
+            h2: {
+              marginTop: 0,
+              color: theme('colors.neutral.500'),
+              fontSize: theme('fontSize.base'),
+              fontWeight: theme('fontWeight.normal'),
+            },
+            small: {
+              color: theme('colors.neutral.500'),
+              fontSize: theme('fontSize.xs'),
+              fontWeight: theme('fontWeight.normal'),
+            },
+            sup: {
+              color: theme('colors.neutral.500'),
+              fontSize: theme('fontSize.xs'),
+              fontWeight: theme('fontWeight.normal'),
+            },
+          },
+        },
+      }),
+    },
   },
-  plugins: [],
+  plugins: [typography],
 };
 
 export default config;

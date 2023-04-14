75fb9272148beb309ea1120a353b05abde306405
Hayden Bleasel <hello@haydenbleasel.com>
2023-04-14 17:07:33 -0400

    Restore redirects and secure headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,33 @@
+import { createSecureHeaders } from 'next-secure-headers';
+import redirects from './redirects.json' assert { type: 'json' };
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   experimental: {
     appDir: true,
     typedRoutes: true,
+  },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
+  headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          ...createSecureHeaders(),
+          // HSTS Preload: https://hstspreload.org/
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+        ],
+      },
+    ];
+  },
+  redirects() {
+    return redirects;
   },
 };
 

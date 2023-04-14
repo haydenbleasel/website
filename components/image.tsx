aba9b97f8aa6610e81625e2885193c7ae478aeae
Hayden Bleasel <hello@haydenbleasel.com>
2023-04-14 15:09:09 -0400

    Add built-in selection avoidance to images

--- components/image.tsx
+++ components/image.tsx
@@ -1,7 +1,14 @@
+import clsx from 'clsx';
 import type { ImageProps } from 'next/image';
 import NextImage from 'next/image';
 import type { FC } from 'react';
 
-const Image: FC<ImageProps> = (props) => <NextImage {...props} quality={100} />;
+const Image: FC<ImageProps> = ({ className, ...props }) => (
+  <NextImage
+    className={clsx('select-none', className)}
+    {...props}
+    quality={100}
+  />
+);
 
 export default Image;

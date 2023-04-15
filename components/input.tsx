087546ffd5dde5195927d4db8d572b72c38ccc46
Hayden Bleasel <hello@haydenbleasel.com>
2023-04-15 01:43:13 -0400

    Transition shadows on input and textarea

--- components/input.tsx
+++ components/input.tsx
@@ -27,7 +27,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
         <input
           id={id}
           className={clsx(
-            'flex h-10 w-full rounded-md border border-neutral-300 bg-transparent px-3 py-2 text-sm placeholder:text-neutral-400 focus:outline-none focus:ring-2 focus:ring-neutral-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-700 dark:text-neutral-50 dark:focus:ring-neutral-400 dark:focus:ring-offset-neutral-950',
+            'flex h-10 w-full rounded-md border border-neutral-300 bg-transparent px-3 py-2 text-sm transition-shadow placeholder:text-neutral-400 focus:outline-none focus:ring-2 focus:ring-neutral-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-700 dark:text-neutral-50 dark:focus:ring-neutral-400 dark:focus:ring-offset-neutral-950',
             className
           )}
           ref={ref}

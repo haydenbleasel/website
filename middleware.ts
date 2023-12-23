d039097929afcb31152d96dcf0c7814ad5de7a3a
Hayden Bleasel <hello@haydenbleasel.com>
2023-12-23 17:18:50 -0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,12 +3,12 @@ import type { NextMiddleware, NextRequest } from 'next/server';
 
 export const middleware: NextMiddleware = (request: NextRequest) => {
   if (request.headers.get('host')?.includes('presumi.com')) {
-    const url = new URL('/apps/presumi', process.env.NEXT_PUBLIC_SITE_URL);
+    const url = new URL('/blog/presumi', process.env.NEXT_PUBLIC_SITE_URL);
     return NextResponse.redirect(url);
   }
 
   if (request.headers.get('host')?.includes('tryneutral.com')) {
-    const url = new URL('/apps/neutral', process.env.NEXT_PUBLIC_SITE_URL);
+    const url = new URL('/blog/neutral', process.env.NEXT_PUBLIC_SITE_URL);
     return NextResponse.redirect(url);
   }
 

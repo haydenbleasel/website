e7f7aa78fc3e5189c33c4a72d2f0c8f805a2b41a
Hayden Bleasel <hello@haydenbleasel.com>
2024-01-14 20:41:12 -0500

    Improve games page

--- app/gaming/page.tsx
+++ app/gaming/page.tsx
@@ -1,12 +1,15 @@
 import { ClockIcon, StarIcon } from '@radix-ui/react-icons';
 import Image from 'next/image';
 import { Link } from '@/components/link';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Container } from '@/components/container';
-import { getOwnedGames, getPlayerAchievements } from '@/lib/steam';
+import {
+  getOwnedGames,
+  getPlayerAchievements,
+  getRecentGames,
+} from '@/lib/steam';
 import { createMetadata } from '@/lib/metadata';
+import type { BaseSteamGame } from '@/lib/steam';
 import type { Metadata } from 'next';
-import type { SteamGame } from '@/lib/steam';
 import type { ReactElement } from 'react';
 
 const title = 'Games';
@@ -21,7 +24,7 @@ export const metadata: Metadata = createMetadata({
 const Game = async ({
   data,
 }: {
-  readonly data: SteamGame;
+  readonly data: BaseSteamGame;
 }): Promise<ReactElement> => {
   const stats = await getPlayerAchievements(data.appid);
   let imageUrl: string | null =
@@ -40,58 +43,80 @@ const Game = async ({
   return (
     <Link
       href={`https://steamcommunity.com/app/${data.appid}`}
-      key={data.name}
-      className="no-underline hover:-translate-y-1 transition-transform"
+      key={data.appid}
+      className="no-underline items-center flex gap-4 hover:bg-zinc-100 dark:hover:bg-zinc-800 p-4 rounded-md transition-colors"
     >
-      <Card className="not-prose overflow-hidden bg-white dark:bg-zinc-800">
-        {imageUrl ? (
-          <Image
-            src={imageUrl}
-            alt=""
-            width={1920}
-            height={620}
-            className="aspect-[1920/620] object-cover"
-          />
-        ) : (
-          <div className="aspect-[1920/620] bg-zinc-500 dark:bg-zinc-400" />
-        )}
-        <CardHeader>
-          <CardTitle className="leading-tight">{data.name}</CardTitle>
-        </CardHeader>
-        <CardContent className="flex items-center justify-between gap-4">
-          <div className="flex items-center gap-4 text-sm text-zinc-500 dark:text-zinc-400 font-medium">
+      {imageUrl ? (
+        <Image
+          src={imageUrl}
+          alt=""
+          width={99}
+          height={32}
+          className="rounded-md shrink-0"
+        />
+      ) : (
+        <div className="w-[99px] h-8 rounded-md bg-zinc-200 dark:bg-zinc-800" />
+      )}
+      <div>
+        <div className="truncate">
+          <p className="text-sm text-zinc-900 dark:text-zinc-100 font-medium truncate">
+            {data.name}
+          </p>
+        </div>
+        <div className="flex items-center gap-3 text-xs text-zinc-500 dark:text-zinc-400 font-medium">
+          <span className="flex items-center gap-1">
+            <ClockIcon className="w-3 h-3 shrink-0" />
+            {Math.round(data.playtime_forever / 60)} hours
+          </span>
+          {stats?.achievements?.length ? (
             <span className="flex items-center gap-1">
-              <ClockIcon className="w-3 h-3" />
-              {Math.round(data.playtime_forever / 60)} hours
+              <StarIcon className="w-3 h-3 shrink-0" />
+              {stats.achievements.length} trophies
             </span>
-            {stats?.achievements?.length ? (
-              <span className="flex items-center gap-1">
-                <StarIcon className="w-3 h-3" />
-                {stats.achievements.length} achievements
-              </span>
-            ) : null}
-          </div>
-        </CardContent>
-      </Card>
+          ) : null}
+        </div>
+      </div>
     </Link>
   );
 };
 
 const Games = async (): Promise<ReactElement> => {
-  const games = await getOwnedGames();
+  const allGames = await getOwnedGames();
+  const recentGames = await getRecentGames();
+
+  const collections = [
+    {
+      name: 'Recently Played',
+      games: recentGames,
+    },
+    {
+      name: 'All Games',
+      games: allGames.sort(
+        (gameA, gameB) => gameB.playtime_forever - gameA.playtime_forever
+      ),
+    },
+  ];
 
   return (
     <Container wide>
       <h1 className="mb-0">{title}</h1>
       <p>{description}</p>
-      <div className="mt-8 grid sm:grid-cols-2 gap-8">
-        {games
-          .sort(
-            (gameA, gameB) => gameB.playtime_forever - gameA.playtime_forever
-          )
-          .map((game) => (
-            <Game data={game} key={game.appid} />
-          ))}
+      <div className="mt-8 grid gap-8 not-prose">
+        {Object.values(collections).map(({ name, games }) => (
+          <div
+            className="border border-zinc-200 dark:border-zinc-800 rounded-md p-4 space-y-4"
+            key={name}
+          >
+            <p className="text-zinc-900 dark:text-zinc-100 font-semibold">
+              {name}
+            </p>
+            <div className="grid md:grid-cols-2">
+              {games.map((game) => (
+                <Game data={game} key={game.appid} />
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
     </Container>
   );
--- lib/steam.ts
+++ lib/steam.ts
@@ -1,20 +1,21 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 
-export type SteamGame = {
+export type BaseSteamGame = {
   appid: number;
   name: string;
+  playtime_2weeks: number;
   playtime_forever: number;
   img_icon_url: string;
-  content_descriptorids?: number[];
-  playtime_2weeks?: number;
-  has_community_visible_stats?: boolean;
-  has_leaderboards?: boolean;
 };
 
 type GetOwnedGamesResponse = {
   response: {
     game_count: number;
-    games: SteamGame[];
+    games: (BaseSteamGame & {
+      content_descriptorids?: number[];
+      has_community_visible_stats?: boolean;
+      has_leaderboards?: boolean;
+    })[];
   };
 };
 
@@ -28,6 +29,17 @@ type GetPlayerAchievementsResponse = {
       unlocktime: number;
     }[];
     success: boolean;
+  };
+};
+
+type GetRecentlyPlayedGamesResponse = {
+  response: {
+    total_count: number;
+    games: (BaseSteamGame & {
+      playtime_windows_forever: number;
+      playtime_mac_forever: number;
+      playtime_linux_forever: number;
+    })[];
   };
 };
 
@@ -61,6 +73,24 @@ export const getOwnedGames = async (): Promise<
   );
 
   const data = (await response.json()) as GetOwnedGamesResponse;
+
+  return data.response.games.filter(
+    ({ playtime_forever }) => playtime_forever > 60
+  );
+};
+
+export const getRecentGames = async (): Promise<
+  GetRecentlyPlayedGamesResponse['response']['games']
+> => {
+  if (!process.env.STEAM_API_KEY) {
+    throw new Error('Missing Steam API key');
+  }
+
+  const response = await fetch(
+    `http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${process.env.STEAM_API_KEY}&steamid=${steamId}&format=json`
+  );
+
+  const data = (await response.json()) as GetRecentlyPlayedGamesResponse;
 
   return data.response.games.filter(
     ({ playtime_forever }) => playtime_forever > 60

473ce90fc430a651d0d08c0df748eb2703ee8e43
Hayden Bleasel <hello@haydenbleasel.com>
2024-04-08 12:37:45 +0200

    Add rate limiter to contact form

--- app/(pages)/contact/actions/contact.ts
+++ app/(pages)/contact/actions/contact.ts
@@ -3,6 +3,9 @@
 'use server';
 
 import { revalidatePath } from 'next/cache';
+import { kv } from '@vercel/kv';
+import { Ratelimit } from '@upstash/ratelimit';
+import { headers } from 'next/headers';
 import { resend } from '@/lib/resend';
 import { parseError } from '@/lib/utils';
 import { formSchema } from '@/lib/form';
@@ -13,6 +16,15 @@ const to = process.env.RESEND_TO;
 if (!from || !to) {
   throw new Error(
     'RESEND_FROM and RESEND_TO environment variables are not set'
+  );
+}
+
+const kvRestApiUrl = process.env.KV_REST_API_URL;
+const kvRestApiToken = process.env.KV_REST_API_TOKEN;
+
+if (!kvRestApiUrl || !kvRestApiToken) {
+  console.warn(
+    'KV_REST_API_URL and KV_REST_API_TOKEN environment variables are not set'
   );
 }
 
@@ -27,6 +39,21 @@ export const contact = async (
   console.log('📧 Contact form submission', data);
 
   try {
+    const ip = headers().get('x-forwarded-for');
+    const ratelimit = new Ratelimit({
+      redis: kv,
+      // rate limit to 1 request every day
+      limiter: Ratelimit.slidingWindow(1, '1d'),
+    });
+
+    const { success } = await ratelimit.limit(`ratelimit_${ip}`);
+
+    if (!success) {
+      throw new Error(
+        'You have reached your request limit. Please try again later.'
+      );
+    }
+
     console.log('📧 Sending email...');
 
     const response = await resend.emails.send({

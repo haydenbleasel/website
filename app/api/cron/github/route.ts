31ba9c3df9508ca6c0caa97c2e7879c38eb979f2
Hayden Bleasel <hello@haydenbleasel.com>
2024-08-25 12:42:32 -0400

    Simplify logic

--- app/api/cron/github/route.ts
+++ app/api/cron/github/route.ts
@@ -15,33 +15,28 @@ export const dynamic = 'force-dynamic';
 const today = new Date();
 const endOfLastWeek = endOfWeek(subDays(today, 7));
 const startOf26WeeksAgo = startOfWeek(subWeeks(endOfLastWeek, 26));
+const oneYearAgo = subYears(today, 1);
 
 export const GET = async (): Promise<Response> => {
   try {
     const response = await fetch('https://github-contributions-api.jogruber.de/v4/haydenbleasel');
-    const data = (await response.json()) as {
-      total: Record<number, string>,
-      contributions: Activity[];
-    };
+    const { contributions } = await response.json() as { contributions: Activity[] };
     
-    const filteredData = data.contributions.filter((activity) => {
-      const activityDate = new Date(activity.date);
-      return activityDate <= endOfLastWeek && activityDate >= startOf26WeeksAgo;
-    });
-
-    const oneYearAgo = subYears(new Date(), 1);
-    const total = data.contributions
-      .filter((activity) => new Date(activity.date) >= oneYearAgo)
-      .reduce((newTotal, { count }) => newTotal + count, 0);
-
-    const content: GitHubProperties = { data: filteredData, total };
+    const content: GitHubProperties = {
+      data: contributions.filter(activity => {
+        const activityDate = new Date(activity.date);
+        
+        return activityDate <= endOfLastWeek && activityDate >= startOf26WeeksAgo;
+      }),
+      total: contributions.reduce((total, { date, count }) => 
+        new Date(date) >= oneYearAgo ? total + count : total, 0
+      )
+    };
 
     await updateEdgeConfig('github', content);
 
     return new Response(undefined, { status: 204 });
   } catch (error) {
-    const message = parseError(error);
-
-    return new Response(message, { status: 500 });
+    return new Response(parseError(error), { status: 500 });
   }
 };

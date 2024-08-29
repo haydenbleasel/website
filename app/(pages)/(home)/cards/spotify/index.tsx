b5b88d031bf8de93c35c2ae418ff457fa0191e5e
Hayden Bleasel <hello@haydenbleasel.com>
2024-08-29 12:11:22 -0400

    Update index.tsx

--- app/(pages)/(home)/cards/spotify/index.tsx
+++ app/(pages)/(home)/cards/spotify/index.tsx
@@ -36,7 +36,7 @@ const SpotifyCard = async (): Promise<ReactElement> => {
           width={640}
           height={640}
           className="absolute rounded-full h-14 w-14 shrink-0 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-          sizes="48px"
+          sizes="56px"
           quality={100}
         />
       </div>

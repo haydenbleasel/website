6f9ca95c57aef462dc0b48790192162e8cbfdb3f
Hayden Bleasel <hello@haydenbleasel.com>
2024-09-30 14:20:49 -0400

    Use Edge Storage for video url

--- app/(pages)/(home)/cards/video.tsx
+++ app/(pages)/(home)/cards/video.tsx
@@ -1,9 +1,18 @@
 import { Card } from '@/components/card';
 import { Video } from '@/components/video';
+import { get } from '@vercel/edge-config';
 import type { ReactElement } from 'react';
 
-export const VideoCard = async (): Promise<ReactElement> => (
-  <Card title="Latest Drone Video">
-    <Video url="https://youtu.be/5NOgrMM72k8" controls />
-  </Card>
-);
+export const VideoCard = async (): Promise<ReactElement> => {
+  const video = await get<string>('video');
+
+  if (!video) {
+    return <div />;
+  }
+
+  return (
+    <Card title="Latest Drone Video">
+      <Video url={video} controls />
+    </Card>
+  );
+};

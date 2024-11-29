4f6342cd95f36fac29a495cfa0419497adfa5793
Hayden Bleasel <hello@haydenbleasel.com>
2024-11-29 12:22:32 -0500

    Scaffold header with logo

---
+++ app/components/header/index.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import Logo from './logo.jpg';
+
+export const Header = () => (
+  <header className="container mx-auto flex items-center justify-between">
+    <Link href="/" aria-label="Hayden Bleasel">
+      <Image
+        src={Logo}
+        alt=""
+        width={32}
+        height={32}
+        className="h-8 w-8 overflow-hidden rounded-full object-cover"
+        priority
+        placeholder="blur"
+      />
+    </Link>
+    <nav>
+      <Link href="/">Home</Link>
+      <Link href="/projects">Projects</Link>
+      <Link href="/blog">Blog</Link>
+    </nav>
+    <button>Get in touch</button>
+  </header>
+);

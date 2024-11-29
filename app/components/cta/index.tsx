ec5f73c0f0b541f96fe1b5d53462fb4656e36c45
Hayden Bleasel <hello@haydenbleasel.com>
2024-11-29 11:31:53 -0500

    Show dynamic contacts count

--- app/components/cta/index.tsx
+++ app/components/cta/index.tsx
@@ -1,8 +1,18 @@
+import { resend } from '@/lib/resend';
 import { Newsletter } from './newsletter';
 
-export const CallToAction = () => (
-  <div className="bg-secondary">
-    <p>Join 2200+ readers and get infrequent updates on new projects.</p>
-    <Newsletter />
-  </div>
-);
+export const CallToAction = async () => {
+  const contacts = await resend.contacts.list({
+    audienceId: process.env.RESEND_AUDIENCE_ID,
+  });
+
+  return (
+    <div className="bg-secondary">
+      <p>
+        Join {contacts.data?.data.length} readers and get infrequent updates on
+        new projects.
+      </p>
+      <Newsletter />
+    </div>
+  );
+};

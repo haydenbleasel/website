0cdc3fe35be4d06dfa8dedd6210164948e05f3d5
Hayden Bleasel <hello@haydenbleasel.com>
2024-11-30 14:38:44 -0500

    Update index.tsx

--- components/cta/index.tsx
+++ components/cta/index.tsx
@@ -2,7 +2,7 @@ import { Newsletter } from './newsletter';
 import { Teaser } from './teaser';
 
 export const CallToAction = () => (
-  <section className="grid grid-cols-2 gap-8 p-8">
+  <section className="grid grid-cols-2 gap-8 bg-dashed p-8">
     <Teaser />
     <Newsletter />
   </section>

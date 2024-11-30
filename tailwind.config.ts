7fcb4552012174733bcffc621092a89a8fb2178f
Hayden Bleasel <hello@haydenbleasel.com>
2024-11-30 10:03:50 -0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,7 @@ export default {
       colors: {
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
+        backdrop: 'hsl(var(--backdrop))',
         card: {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',

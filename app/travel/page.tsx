a05df3f6e273e7f497c68dea776367a6f82e556a
Hayden Bleasel <hello@haydenbleasel.com>
2024-11-29 22:38:02 -0500

    Scaffold Travel page

---
+++ app/travel/page.tsx
@@ -0,0 +1,5 @@
+const Travel = () => {
+  return <div>Travel</div>;
+};
+
+export default Travel;
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -7,6 +7,7 @@ const links = [
   { href: '/', label: 'Home' },
   { href: '/work', label: 'Work' },
   { href: '/projects', label: 'Projects' },
+  { href: '/travel', label: 'Travel' },
   { href: '/blog', label: 'Blog' },
   { href: '/stack', label: 'Stack' },
   { href: '/features', label: 'Features' },

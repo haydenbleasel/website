40dd32d8d55a34692f287944a82eb256797b6a70
Hayden Bleasel <hello@haydenbleasel.com>
2024-11-30 10:01:26 -0500

    Update github-activity.tsx

--- app/(home)/components/github-activity.tsx
+++ app/(home)/components/github-activity.tsx
@@ -12,37 +12,32 @@ export const GitHubActivity = async (): Promise<ReactElement> => {
   }
 
   return (
-    <section>
-      <p>GitHub Activity</p>
-      <div className="flex flex-col gap-4">
-        <p className="text-neutral-500 text-sm dark:text-neutral-400">
-          {github.total} contributions in the last year
-        </p>
-        <div className="flex flex-col gap-[3px]">
-          <ActivityCalendar
-            hideColorLegend
-            hideTotalCount
-            hideMonthLabels
-            data={github.data}
-            theme={{
-              light: [
-                tailwind.theme.colors.neutral[200],
-                tailwind.theme.colors.orange[200],
-                tailwind.theme.colors.orange[400],
-                tailwind.theme.colors.orange[600],
-                tailwind.theme.colors.orange[800],
-              ],
-              dark: [
-                tailwind.theme.colors.neutral[700],
-                tailwind.theme.colors.orange[200],
-                tailwind.theme.colors.orange[400],
-                tailwind.theme.colors.orange[600],
-                tailwind.theme.colors.orange[800],
-              ],
-            }}
-          />
-        </div>
-      </div>
+    <section className="relative">
+      <ActivityCalendar
+        hideColorLegend
+        hideTotalCount
+        hideMonthLabels
+        data={github.data}
+        theme={{
+          light: [
+            tailwind.theme.colors.neutral[200],
+            tailwind.theme.colors.green[200],
+            tailwind.theme.colors.green[400],
+            tailwind.theme.colors.green[600],
+            tailwind.theme.colors.green[800],
+          ],
+          dark: [
+            tailwind.theme.colors.neutral[700],
+            tailwind.theme.colors.green[200],
+            tailwind.theme.colors.green[400],
+            tailwind.theme.colors.green[600],
+            tailwind.theme.colors.green[800],
+          ],
+        }}
+      />
+      <p className="absolute bottom-4 left-4 text-neutral-500 text-sm dark:text-neutral-400">
+        {github.total} contributions in the last year
+      </p>
     </section>
   );
 };

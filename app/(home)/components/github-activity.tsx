70ea70144a265f0f503868a544bf4f7cf9a2888b
Hayden Bleasel <hello@haydenbleasel.com>
2024-11-30 14:28:26 -0500

    Update GitHub activity

--- app/(home)/components/github-activity.tsx
+++ app/(home)/components/github-activity.tsx
@@ -1,23 +1,28 @@
-import type { GitHubProperties } from '@/app/api/cron/github/route';
+import { getActivity } from '@/app/actions/github';
+import { social } from '@/lib/social';
 import tailwind from '@/lib/tailwind';
-import { get } from '@vercel/edge-config';
 import type { ReactElement } from 'react';
 import ActivityCalendar from 'rsc-activity-calendar';
 
 export const GitHubActivity = async (): Promise<ReactElement> => {
-  const github = await get<GitHubProperties>('github');
+  const github = await getActivity();
 
-  if (!github) {
+  if ('error' in github) {
     return <div />;
   }
 
+  const halfLength = Math.floor(github.data.length / 2);
+  const firstHalfData = github.data.slice(0, halfLength);
+  const secondHalfData = github.data.slice(halfLength);
+
   return (
-    <section className="relative">
+    <section className="relative flex flex-col gap-[3px] p-8">
       <ActivityCalendar
         hideColorLegend
         hideTotalCount
         hideMonthLabels
-        data={github.data}
+        data={firstHalfData}
+        weekStart={1}
         theme={{
           light: [
             tailwind.theme.colors.neutral[200],
@@ -34,10 +39,45 @@ export const GitHubActivity = async (): Promise<ReactElement> => {
             tailwind.theme.colors.green[800],
           ],
         }}
+        style={{
+          width: 'auto',
+        }}
       />
-      <p className="absolute bottom-4 left-4 text-neutral-500 text-sm dark:text-neutral-400">
+      <ActivityCalendar
+        hideColorLegend
+        hideTotalCount
+        hideMonthLabels
+        data={secondHalfData}
+        weekStart={1}
+        theme={{
+          light: [
+            tailwind.theme.colors.neutral[200],
+            tailwind.theme.colors.green[200],
+            tailwind.theme.colors.green[400],
+            tailwind.theme.colors.green[600],
+            tailwind.theme.colors.green[800],
+          ],
+          dark: [
+            tailwind.theme.colors.neutral[700],
+            tailwind.theme.colors.green[200],
+            tailwind.theme.colors.green[400],
+            tailwind.theme.colors.green[600],
+            tailwind.theme.colors.green[800],
+          ],
+        }}
+        style={{
+          width: 'auto',
+        }}
+      />
+      <div className="absolute right-0 bottom-8 left-0 z-10 h-40 bg-gradient-to-b from-transparent to-backdrop" />
+      <a
+        className="-translate-x-1/2 absolute bottom-4 left-1/2 z-10 font-mono text-muted-foreground text-xs"
+        href={social.github.href}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         {github.total} contributions in the last year
-      </p>
+      </a>
     </section>
   );
 };
---
+++ app/actions/github.ts
@@ -0,0 +1,51 @@
+import { parseError } from '@/lib/utils';
+import { endOfWeek, subDays, subWeeks } from 'date-fns';
+import ky from 'ky';
+import type { Activity } from 'rsc-activity-calendar';
+
+export type GitHubProperties = {
+  total: number;
+  data: Activity[];
+};
+
+export const maxDuration = 300;
+export const revalidate = 0;
+export const dynamic = 'force-dynamic';
+
+const today = endOfWeek(subWeeks(new Date(), 1));
+const oneYearAgo = subDays(today, 365);
+const twoYearsAgo = subDays(today, 1092);
+
+export const getActivity = async (): Promise<
+  GitHubProperties | { error: string }
+> => {
+  try {
+    const response = await ky
+      .get<{ contributions: Activity[] }>(
+        'https://github-contributions-api.jogruber.de/v4/haydenbleasel'
+      )
+      .json();
+
+    const content: GitHubProperties = {
+      data: response.contributions
+        .filter(({ date }) => {
+          const dateObj = new Date(date);
+          return dateObj <= today && dateObj >= twoYearsAgo;
+        })
+        .sort(
+          (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
+        ),
+      total: response.contributions.reduce(
+        (total, { date, count }) =>
+          new Date(date) >= oneYearAgo && new Date(date) <= today
+            ? total + count
+            : total,
+        0
+      ),
+    };
+
+    return content;
+  } catch (error) {
+    return { error: parseError(error) };
+  }
+};

17f7ba372074d2525a1b975b2d0fef67ad78b6e4
Hayden Bleasel <hello@haydenbleasel.com>
2024-12-03 18:29:48 -0500

    More animations

--- components/cta/index.tsx
+++ components/cta/index.tsx
@@ -1,9 +1,21 @@
+import { ViewAnimation } from '@/providers/view-animation';
 import { Newsletter } from './newsletter';
 import { Teaser } from './teaser';
 
 export const CallToAction = () => (
   <section className="grid grid-cols-2 gap-8 bg-dashed p-8">
-    <Teaser />
-    <Newsletter />
+    <ViewAnimation
+      initial={{ opacity: 0, translateY: -8 }}
+      whileInView={{ opacity: 1, translateY: 0 }}
+    >
+      <Teaser />
+    </ViewAnimation>
+    <ViewAnimation
+      initial={{ opacity: 0, translateY: -8 }}
+      whileInView={{ opacity: 1, translateY: 0 }}
+      delay={0.4}
+    >
+      <Newsletter />
+    </ViewAnimation>
   </section>
 );

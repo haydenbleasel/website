7eacb5b354a5063626a40cb7a7d56fdb60dea46a
Hayden Bleasel <hello@haydenbleasel.com>
2024-12-03 19:08:19 -0500

    Update steam.tsx

--- app/(home)/components/steam.tsx
+++ app/(home)/components/steam.tsx
@@ -18,15 +18,45 @@ export const Steam = async (): Promise<ReactElement> => {
         target="_blank"
         rel="noreferrer noopener"
       >
-        <Image
-          src={game.image}
-          alt=""
-          width={460}
-          height={215}
-          className="m-0 h-auto w-full max-w-[230px] shrink-0 rounded"
-          sizes="230px"
-          quality={100}
-        />
+        <div className="relative w-fit">
+          <Image
+            src={game.image}
+            alt=""
+            width={460}
+            height={215}
+            className="m-0 h-auto w-full max-w-[230px] shrink-0 rounded"
+            sizes="230px"
+            quality={100}
+          />
+          <div
+            className="-left-4 -right-4 -bottom-0.5 absolute h-px border-b border-dashed"
+            style={{
+              maskImage:
+                'linear-gradient(to right, transparent, hsl(var(--foreground)) 2rem, hsl(var(--foreground)) calc(100% - 2rem), transparent)',
+            }}
+          />
+          <div
+            className="-left-4 -right-4 -top-0.5 absolute h-px border-t border-dashed"
+            style={{
+              maskImage:
+                'linear-gradient(to right, transparent, hsl(var(--foreground)) 2rem, hsl(var(--foreground)) calc(100% - 2rem), transparent)',
+            }}
+          />
+          <div
+            className="-top-4 -bottom-4 -left-0.5 absolute w-px border-l border-dashed"
+            style={{
+              maskImage:
+                'linear-gradient(to bottom, transparent, hsl(var(--foreground)) 2rem, hsl(var(--foreground)) calc(100% - 2rem), transparent)',
+            }}
+          />
+          <div
+            className="-top-4 -bottom-4 -right-0.5 absolute w-px border-r border-dashed"
+            style={{
+              maskImage:
+                'linear-gradient(to bottom, transparent, hsl(var(--foreground)) 2rem, hsl(var(--foreground)) calc(100% - 2rem), transparent)',
+            }}
+          />
+        </div>
         <div className="flex flex-col gap-2">
           <small className="text-muted-foreground">Recently played</small>
           <h2 className="font-bold text-3xl tracking-tight">{game.name}</h2>

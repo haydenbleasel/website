de3f64591a996aa576b5baea02708c4bc4e08882
Hayden Bleasel <hello@haydenbleasel.com>
2024-12-09 18:53:36 -0500

    Add redirects

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,62 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+
+  // biome-ignore lint/suspicious/useAwait: "redirects" is async
+  async redirects() {
+    return [
+      {
+        source: '/video',
+        destination: '/travel',
+        permanent: true,
+      },
+      {
+        source: '/mailing-list',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/clients',
+        destination: '/work',
+        permanent: true,
+      },
+      {
+        source: '/apps',
+        destination: '/projects',
+        permanent: true,
+      },
+      {
+        source: '/speaking',
+        destination: '/live',
+        permanent: true,
+      },
+      {
+        source: '/features',
+        destination: '/live',
+        permanent: true,
+      },
+      {
+        source: '/services',
+        destination: '/contact',
+        permanent: true,
+      },
+      {
+        source: '/gaming',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/design',
+        destination: 'https://www.figma.com/@haydenbleasel',
+        permanent: true,
+      },
+      {
+        source: '/code',
+        destination: 'https://github.com/haydenbleasel',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;

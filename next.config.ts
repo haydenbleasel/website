a17fb85b87be0f5efa907136bc2527e87ed5355e
Hayden Bleasel <hello@haydenbleasel.com>
2024-12-09 18:53:30 -0500

    Add secure headers

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from 'next';
+import { createSecureHeaders } from 'next-secure-headers';
 
 const nextConfig: NextConfig = {
   images: {
@@ -10,6 +11,22 @@ const nextConfig: NextConfig = {
       { hostname: 'i.scdn.co', protocol: 'https' },
       { hostname: 'shared.akamai.steamstatic.com', protocol: 'https' },
     ],
+  },
+
+  // biome-ignore lint/suspicious/useAwait: "headers" is async
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: createSecureHeaders({
+          // HSTS Preload: https://hstspreload.org/
+          forceHTTPSRedirect: [
+            true,
+            { maxAge: 63072000, includeSubDomains: true, preload: true },
+          ],
+        }),
+      },
+    ];
   },
 };
 

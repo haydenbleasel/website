16a4559d43158ec232b618aa4e9a41368e89a565
Hayden Bleasel <hello@haydenbleasel.com>
2024-12-09 17:42:55 -0500

    Redesign blog index

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,10 @@
 import { Prose } from '@/components/prose';
 import { Section } from '@/components/section';
 import { Button } from '@/components/ui/button';
-import { cn } from '@/lib/utils';
 import { basehub } from 'basehub';
 import { BaseHubImage } from 'basehub/next-image';
 import { Pump } from 'basehub/react-pump';
+import groupBy from 'lodash.groupby';
 import Link from 'next/link';
 
 export const generateMetadata = async () => {
@@ -62,12 +62,15 @@ const Blog = () => (
       const remainingPosts = data.blog.posts.items.filter(
         (post) => post !== featuredPost
       );
+      const remainingPostsByYear = groupBy(remainingPosts, (post) =>
+        new Date(post.date).getFullYear()
+      );
 
       return (
-        <Section className="grid border-t sm:grid-cols-3">
+        <Section className="divide-y border-t">
           {featuredPost && (
-            <>
-              <div className="bg-background sm:col-span-2 sm:border-b">
+            <div className="grid sm:grid-cols-3">
+              <div className="bg-background sm:col-span-2">
                 {featuredPost.image && (
                   <BaseHubImage
                     className="sm:col-span-2"
@@ -78,7 +81,7 @@ const Blog = () => (
                   />
                 )}
               </div>
-              <div className="flex flex-col items-start justify-between gap-4 border-b p-8">
+              <div className="flex flex-col items-start justify-between gap-4 p-8">
                 <div className="flex flex-col gap-2">
                   <small className="text-muted-foreground">Featured post</small>
                   <h2 className="font-bold text-2xl leading-normal tracking-tight">
@@ -92,35 +95,50 @@ const Blog = () => (
                   <Link href={`/blog/${featuredPost._slug}`}>Read more</Link>
                 </Button>
               </div>
-            </>
+            </div>
           )}
-          {remainingPosts
-            .sort(
-              (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
-            )
-            .map((post, index) => (
-              <Link
-                key={post._title}
-                href={`/blog/${post._slug}`}
-                className={cn(
-                  'flex flex-col gap-2 p-8 transition-colors hover:bg-background',
-                  index % 3 !== 2 && 'border-r',
-                  index > 2 && 'border-t'
-                )}
+          {Object.entries(remainingPostsByYear)
+            .sort((a, b) => Number(b[0]) - Number(a[0]))
+            .map(([year, posts]) => (
+              <div
+                className="grid divide-y sm:grid-cols-3 sm:divide-x sm:divide-y-0"
+                key={year}
               >
-                <h2 className="font-bold text-lg leading-normal tracking-tight">
-                  {post._title}
-                </h2>
-                <Prose className="prose-sm line-clamp-3">
-                  {post.content?.plainText.slice(0, 250)}
-                </Prose>
-                <small className="text-muted-foreground text-xs">
-                  {new Intl.DateTimeFormat('en-US', {
-                    dateStyle: 'medium',
-                  }).format(new Date(post.date))}{' '}
-                  &bull; {post.content?.readingTime} min read
-                </small>
-              </Link>
+                <div className="bg-dashed">
+                  <div className="sticky top-16 p-8">
+                    <h2 className="font-bold text-2xl leading-normal tracking-tight">
+                      {year}
+                    </h2>
+                  </div>
+                </div>
+                <div className="divide-y sm:col-span-2">
+                  {posts
+                    .sort(
+                      (a, b) =>
+                        new Date(b.date).getTime() - new Date(a.date).getTime()
+                    )
+                    .map((post) => (
+                      <Link
+                        key={post._title}
+                        href={`/blog/${post._slug}`}
+                        className="flex flex-col gap-2 p-8 transition-colors hover:bg-background"
+                      >
+                        <h2 className="font-bold text-lg leading-normal tracking-tight">
+                          {post._title}
+                        </h2>
+                        <Prose className="prose-sm line-clamp-3">
+                          {post.content?.plainText.slice(0, 250)}
+                        </Prose>
+                        <small className="text-muted-foreground text-xs">
+                          {new Intl.DateTimeFormat('en-US', {
+                            dateStyle: 'medium',
+                          }).format(new Date(post.date))}{' '}
+                          &bull; {post.content?.readingTime} min read
+                        </small>
+                      </Link>
+                    ))}
+                </div>
+              </div>
             ))}
         </Section>
       );

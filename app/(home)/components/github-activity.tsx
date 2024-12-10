0ba3252ee5496f3e8ea9898e5b107ec41934b622
Hayden Bleasel <hello@haydenbleasel.com>
2024-12-09 21:05:37 -0500

    Update github-activity.tsx

--- app/(home)/components/github-activity.tsx
+++ app/(home)/components/github-activity.tsx
@@ -4,33 +4,7 @@ import { social } from '@/lib/social';
 import tailwind from '@/lib/tailwind';
 import { ViewAnimation } from '@/providers/view-animation';
 import type { ReactElement } from 'react';
-import ActivityCalendar, { type Activity } from 'rsc-activity-calendar';
-
-const Calendar = ({ data }: { data: Activity[] }) => (
-  <ActivityCalendar
-    hideColorLegend
-    hideTotalCount
-    hideMonthLabels
-    data={data}
-    weekStart={1}
-    theme={{
-      light: [
-        tailwind.theme.colors.neutral[200],
-        tailwind.theme.colors.green[200],
-        tailwind.theme.colors.green[400],
-        tailwind.theme.colors.green[600],
-        tailwind.theme.colors.green[800],
-      ],
-      dark: [
-        tailwind.theme.colors.neutral[800],
-        tailwind.theme.colors.green[200],
-        tailwind.theme.colors.green[400],
-        tailwind.theme.colors.green[600],
-        tailwind.theme.colors.green[800],
-      ],
-    }}
-  />
-);
+import ActivityCalendar from 'rsc-activity-calendar';
 
 export const GitHubActivity = async (): Promise<ReactElement> => {
   const github = await getActivity();
@@ -55,10 +29,37 @@ export const GitHubActivity = async (): Promise<ReactElement> => {
         whileInView={{ opacity: 1 }}
         className="relative grid gap-0.5 sm:grid-cols-2 sm:gap-[3px] sm:p-8 lg:gap-1"
       >
-        <Calendar data={thirdQuarterData} />
-        <Calendar data={fourthQuarterData} />
-        <Calendar data={firstQuarterData} />
-        <Calendar data={secondQuarterData} />
+        {[
+          thirdQuarterData,
+          fourthQuarterData,
+          firstQuarterData,
+          secondQuarterData,
+        ].map((data, index) => (
+          <ActivityCalendar
+            key={index}
+            hideColorLegend
+            hideTotalCount
+            hideMonthLabels
+            data={data}
+            weekStart={1}
+            theme={{
+              light: [
+                tailwind.theme.colors.neutral[200],
+                tailwind.theme.colors.green[200],
+                tailwind.theme.colors.green[400],
+                tailwind.theme.colors.green[600],
+                tailwind.theme.colors.green[800],
+              ],
+              dark: [
+                tailwind.theme.colors.neutral[800],
+                tailwind.theme.colors.green[200],
+                tailwind.theme.colors.green[400],
+                tailwind.theme.colors.green[600],
+                tailwind.theme.colors.green[800],
+              ],
+            }}
+          />
+        ))}
         <div className="absolute right-0 bottom-0 left-0 z-10 h-full bg-gradient-to-b from-transparent to-backdrop sm:bottom-8 sm:h-40" />
         <a
           className="-translate-x-1/2 absolute bottom-4 left-1/2 z-10 whitespace-nowrap font-mono text-muted-foreground text-xs"

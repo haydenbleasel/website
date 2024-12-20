8cacfe5e40afadd2d67960f2a2235f228605b07b
Hayden Bleasel <hello@haydenbleasel.com>
2024-12-20 02:13:28 -0500

    Update basehub.ts

--- lib/basehub.ts
+++ lib/basehub.ts
@@ -1,5 +1,7 @@
 import { basehub as basehubClient } from 'basehub';
+import { env } from './env';
 
 export const basehub = basehubClient({
   cache: process.env.NODE_ENV === 'production' ? undefined : 'no-store',
+  token: env.BASEHUB_TOKEN,
 });

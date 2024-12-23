7711ed9f6c6cedb632c96022ef5117e9ae1d9042
Hayden Bleasel <hello@haydenbleasel.com>
2024-12-22 21:44:06 -0600

    Remove explicit cache prop
    `cache` opts the query out of automatic on-demand revalidation.

--- lib/basehub.ts
+++ lib/basehub.ts
@@ -2,6 +2,5 @@ import { basehub as basehubClient } from 'basehub';
 import { env } from './env';
 
 export const basehub = basehubClient({
-  cache: process.env.NODE_ENV === 'production' ? undefined : 'no-store',
   token: env.BASEHUB_TOKEN,
 });

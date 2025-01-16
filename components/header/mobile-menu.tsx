781aa22857c61f4fcb45bc47140c98649bb553ce
Hayden Bleasel <hello@haydenbleasel.com>
2025-01-16 09:32:19 -0500

    Improve mobile menu styling

--- components/header/mobile-menu.tsx
+++ components/header/mobile-menu.tsx
@@ -4,6 +4,7 @@ import { navigation } from '@/lib/navigation';
 import { cn } from '@/lib/utils';
 import { atom, useAtom } from 'jotai';
 import Link from 'next/link';
+import { ActiveLink } from '../active-link';
 import { Button } from '../ui/button';
 
 export const mobileMenuOpen = atom(false);
@@ -14,20 +15,19 @@ export const MobileMenu = () => {
   return (
     <div
       className={cn(
-        'fixed top-[53px] right-0 left-0 z-50 flex h-[calc(100vh-53px)] w-full flex-col gap-4 bg-backdrop p-4 md:p-8',
+        'fixed top-[53px] right-0 left-0 z-50 flex h-[calc(100vh-53px)] flex-col gap-4 bg-backdrop p-4 md:p-8',
         'sm:top-[69px] sm:h-[calc(100vh-69px)]',
         isOpen ? 'flex' : 'hidden'
       )}
     >
       {navigation.map((link) => (
-        <Link
+        <ActiveLink
           key={link.href}
           href={link.href}
-          className="sm:text-lg"
           onClick={() => setIsOpen(false)}
         >
           {link.label}
-        </Link>
+        </ActiveLink>
       ))}
       <Button variant="outline" asChild onClick={() => setIsOpen(false)}>
         <Link href="/contact">Get in touch</Link>

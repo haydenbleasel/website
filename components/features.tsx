a4b3219a126fd7a11fa48d07e55e0714c5cd9c1c
Hayden Bleasel <hello@haydenbleasel.com>
2025-06-12 22:02:33 -0700

    Cleanup

--- components/features.tsx
+++ components/features.tsx
@@ -2,7 +2,7 @@
 
 import { interviews, speaking } from '@/lib/live';
 import { useState } from 'react';
-import { Feature } from './feature';
+import { Link } from './link';
 
 export const Features = () => {
   const [open, setOpen] = useState(false);
@@ -12,7 +12,22 @@ export const Features = () => {
     <>
       <div className="grid gap-3">
         {data.slice(0, open ? data.length : 5).map((feature) => (
-          <Feature key={feature.name} {...feature} />
+          <Link
+            key={feature.name}
+            href={feature.url}
+            className="group flex flex-col gap-1 overflow-hidden text-sm sm:flex-row sm:items-center sm:gap-2"
+          >
+            <span className="flex flex-col gap-1 truncate sm:flex-row sm:items-center sm:gap-2">
+              <p className="truncate text-foreground">{feature.name}</p>
+              <p className="shrink-0 text-foreground-lighter transition-colors group-hover:text-foreground-light">
+                {feature.location}
+              </p>
+            </span>
+            <div className="hidden h-px shrink-0 grow bg-border transition-colors group-hover:bg-border-dark sm:block" />
+            <p className="shrink-0 text-foreground-lighter transition-colors group-hover:text-foreground-light">
+              {feature.year}
+            </p>
+          </Link>
         ))}
       </div>
       {!open && (

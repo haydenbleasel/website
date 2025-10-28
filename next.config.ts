9623079bcb1c5314785a0d551089a1b0695c9e9f
Hayden Bleasel <hello@haydenbleasel.com>
2025-10-28 12:30:57 -0700

    Enable Turbopack filesystem caching

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
 };
 
 export default withContentCollections(nextConfig);

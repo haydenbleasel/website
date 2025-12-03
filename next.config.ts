f6aaf1cd0ae2e08d6e124bce82288b9224e68907
Hayden Bleasel <hello@haydenbleasel.com>
2025-12-02 21:08:06 -0800

    Implement mdx

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
+import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({});
+
+export default withMDX(nextConfig);
